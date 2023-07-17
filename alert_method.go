d801a8178c8981d4c293c1ea9284874e8a773a2b
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-07-17 13:44:46 +0200

    rollback deleted files

---
+++ alert_method.go
@@ -0,0 +1,189 @@
+package nobl9
+
+import "encoding/json"
+
+// AlertMethod represents the configuration required to send a notification to an external service
+// when an alert is triggered.
+type AlertMethod struct {
+	ObjectHeader
+	Spec AlertMethodSpec `json:"spec"`
+}
+
+// PublicAlertMethod represents the configuration required to send a notification to an external service
+// when an alert is triggered.
+type PublicAlertMethod struct {
+	ObjectHeader
+	Spec   PublicAlertMethodSpec    `json:"spec"`
+	Status *PublicAlertMethodStatus `json:"status,omitempty"`
+}
+
+// PublicAlertMethodStatus represents content of Status optional for PublicAlertMethod Object
+type PublicAlertMethodStatus struct {
+	LastTestDate       string `json:"lastTestDate,omitempty"`
+	NextTestPossibleAt string `json:"nextTestPossibleAt,omitempty"`
+}
+
+// AlertMethodSpec represents content of AlertMethod's Spec.
+type AlertMethodSpec struct {
+	Description string                 `json:"description"`
+	Webhook     *WebhookAlertMethod    `json:"webhook,omitempty"`
+	PagerDuty   *PagerDutyAlertMethod  `json:"pagerduty,omitempty"`
+	Slack       *SlackAlertMethod      `json:"slack,omitempty"`
+	Discord     *DiscordAlertMethod    `json:"discord,omitempty"`
+	Opsgenie    *OpsgenieAlertMethod   `json:"opsgenie,omitempty"`
+	ServiceNow  *ServiceNowAlertMethod `json:"servicenow,omitempty"`
+	Jira        *JiraAlertMethod       `json:"jira,omitempty"`
+	Teams       *TeamsAlertMethod      `json:"msteams,omitempty"`
+	Email       *EmailAlertMethod      `json:"email,omitempty"`
+}
+
+// PublicAlertMethodSpec represents content of AlertMethod's Spec without secrets.
+type PublicAlertMethodSpec struct {
+	Description string                       `json:"description"`
+	Webhook     *PublicWebhookAlertMethod    `json:"webhook,omitempty"`
+	PagerDuty   *PublicPagerDutyAlertMethod  `json:"pagerduty,omitempty"`
+	Slack       *PublicSlackAlertMethod      `json:"slack,omitempty"`
+	Discord     *PublicDiscordAlertMethod    `json:"discord,omitempty"`
+	Opsgenie    *PublicOpsgenieAlertMethod   `json:"opsgenie,omitempty"`
+	ServiceNow  *PublicServiceNowAlertMethod `json:"servicenow,omitempty"`
+	Jira        *PublicJiraAlertMethod       `json:"jira,omitempty"`
+	Teams       *PublicTeamsAlertMethod      `json:"msteams,omitempty"`
+	Email       *EmailAlertMethod            `json:"email,omitempty"`
+}
+
+// WebhookAlertMethod represents a set of properties required to send a webhook request.
+type WebhookAlertMethod struct {
+	URL            string          `json:"url"` // Field required when AlertMethod is created.
+	Template       *string         `json:"template,omitempty"`
+	TemplateFields []string        `json:"templateFields,omitempty"` //nolint:lll
+	Headers        []WebhookHeader `json:"headers,omitempty"`
+}
+
+type WebhookHeader struct {
+	Name     string `json:"name"`
+	Value    string `json:"value"`
+	IsSecret bool   `json:"isSecret"`
+}
+
+// PublicWebhookAlertMethod represents a set of properties required to send a webhook request without secrets.
+type PublicWebhookAlertMethod struct {
+	HiddenURL      string          `json:"url"`
+	Template       *string         `json:"template,omitempty"`
+	TemplateFields []string        `json:"templateFields,omitempty"` //nolint:lll
+	Headers        []WebhookHeader `json:"headers,omitempty"`
+}
+
+// SendResolution If user set SendResolution, then “Send a notification after the cooldown period is over"
+type SendResolution struct {
+	Message *string `json:"message"`
+}
+
+// PagerDutyAlertMethod represents a set of properties required to open an Incident in PagerDuty.
+type PagerDutyAlertMethod struct {
+	IntegrationKey string          `json:"integrationKey"`
+	SendResolution *SendResolution `json:"sendResolution,omitempty"`
+}
+
+// PublicPagerDutyAlertMethod represents a set of properties required to open an Incident in PagerDuty without secrets.
+type PublicPagerDutyAlertMethod struct {
+	HiddenIntegrationKey string          `json:"integrationKey"`
+	SendResolution       *SendResolution `json:"sendResolution,omitempty"`
+}
+
+// SlackAlertMethod represents a set of properties required to send message to Slack.
+type SlackAlertMethod struct {
+	URL string `json:"url"` // Required when AlertMethod is created.
+}
+
+// PublicSlackAlertMethod represents a set of properties required to send message to Slack without secrets.
+type PublicSlackAlertMethod struct {
+	HiddenURL string `json:"url"`
+}
+
+// OpsgenieAlertMethod represents a set of properties required to send message to Opsgenie.
+type OpsgenieAlertMethod struct {
+	Auth string `json:"auth"` // Field required when AlertMethod is created.
+	URL  string `json:"url"`
+}
+
+// PublicOpsgenieAlertMethod represents a set of properties required to send message to Opsgenie without secrets.
+type PublicOpsgenieAlertMethod struct {
+	HiddenAuth string `json:"auth"`
+	URL        string `json:"url"`
+}
+
+// ServiceNowAlertMethod represents a set of properties required to send message to ServiceNow.
+type ServiceNowAlertMethod struct {
+	Username     string `json:"username"`
+	Password     string `json:"password"` // Field required when AlertMethod is created.
+	InstanceName string `json:"instanceName"`
+}
+
+// PublicServiceNowAlertMethod represents a set of properties required to send message to ServiceNow without secrets.
+type PublicServiceNowAlertMethod struct {
+	Username       string `json:"username"`
+	InstanceName   string `json:"instanceName"`
+	HiddenPassword string `json:"password"`
+}
+
+// DiscordAlertMethod represents a set of properties required to send message to Discord.
+type DiscordAlertMethod struct {
+	URL string `json:"url"` // Field required when AlertMethod is created.
+}
+
+// PublicDiscordAlertMethod represents a set of properties required to send message to Discord without secrets.
+type PublicDiscordAlertMethod struct {
+	HiddenURL string `json:"url"`
+}
+
+// JiraAlertMethod represents a set of properties required create tickets in Jira.
+type JiraAlertMethod struct {
+	URL        string `json:"url"`
+	Username   string `json:"username"`
+	APIToken   string `json:"apiToken"` // Field required when AlertMethod is created.
+	ProjectKey string `json:"projectKey"`
+}
+
+// PublicJiraAlertMethod represents a set of properties required create tickets in Jira without secrets.
+type PublicJiraAlertMethod struct {
+	URL            string `json:"url"`
+	Username       string `json:"username"`
+	ProjectKey     string `json:"projectKey"`
+	HiddenAPIToken string `json:"apiToken"`
+}
+
+// TeamsAlertMethod represents a set of properties required create Microsoft Teams notifications.
+type TeamsAlertMethod struct {
+	URL string `json:"url"`
+}
+
+// PublicTeamsAlertMethod represents a set of properties required create Microsoft Teams notifications.
+type PublicTeamsAlertMethod struct {
+	HiddenURL string `json:"url"`
+}
+
+// EmailAlertMethod represents a set of properties required to send an email.
+type EmailAlertMethod struct {
+	To      []string `json:"to,omitempty"`
+	Cc      []string `json:"cc,omitempty"`
+	Bcc     []string `json:"bcc,omitempty"`
+	Subject string   `json:"subject"`
+	Body    string   `json:"body"`
+}
+
+// genericToAlertMethod converts ObjectGeneric to ObjectAlertMethod
+func genericToAlertMethod(o ObjectGeneric, onlyHeader bool) (AlertMethod, error) {
+	res := AlertMethod{
+		ObjectHeader: o.ObjectHeader,
+	}
+	if onlyHeader {
+		return res, nil
+	}
+	var resSpec AlertMethodSpec
+	if err := json.Unmarshal(o.Spec, &resSpec); err != nil {
+		err = EnhanceError(o, err)
+		return res, err
+	}
+	res.Spec = resSpec
+	return res, nil
+}
---
+++ objects.go
@@ -0,0 +1,229 @@
+// Package nobl9 provide an abstraction for communication with API
+package nobl9
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+)
+
+// APIVersion is a value of valid apiVersions
+const (
+	APIVersion = "n9/v1alpha"
+)
+
+// HiddenValue can be used as a value of a secret field and is ignored during saving
+const HiddenValue = "[hidden]"
+
+// Possible values of field kind for valid Objects.
+const (
+	KindSLO         = "SLO"
+	KindService     = "Service"
+	KindAgent       = "Agent"
+	KindProject     = "Project"
+	KindAlertPolicy = "AlertPolicy"
+	KindAlert       = "Alert"
+	KindAlertMethod = "AlertMethod"
+	KindDirect      = "Direct"
+	KindDataExport  = "DataExport"
+	KindRoleBinding = "RoleBinding"
+)
+
+// APIObjects - all Objects available for this version of API
+// Sorted in order of applying
+type APIObjects struct {
+	SLOs          []SLO
+	Services      []Service
+	Agents        []Agent
+	AlertPolicies []AlertPolicy
+	Alerts        []Alert
+	AlertMethods  []AlertMethod
+	Directs       []Direct
+	DataExports   []DataExport
+	Projects      []Project
+	RoleBindings  []RoleBinding
+}
+
+type Payload struct {
+	objects []AnyJSONObj
+}
+
+func (p *Payload) AddObject(in interface{}) {
+	p.objects = append(p.objects, toAnyJSONObj(in))
+}
+
+func (p *Payload) GetObjects() []AnyJSONObj {
+	return p.objects
+}
+
+func toAnyJSONObj(in interface{}) AnyJSONObj {
+	tmp, err := json.Marshal(in)
+	if err != nil {
+		panic(err)
+	}
+	var out AnyJSONObj
+	if err := json.Unmarshal(tmp, &out); err != nil {
+		panic(err)
+	}
+	return out
+}
+
+// UnsupportedKindErr returns appropriate error for missing value in field kind
+// for not empty field kind returns always that is not supported for this apiVersion
+// so have to be validated before
+func UnsupportedKindErr(o ObjectGeneric) error {
+	if strings.TrimSpace(o.Kind) == "" {
+		return EnhanceError(o, errors.New("missing or empty field kind for an Object"))
+	}
+	return EnhanceError(o, fmt.Errorf("invalid Object kind: %s for apiVersion: %s", o.Kind, o.APIVersion))
+}
+
+// ObjectInternal represents part of object which is only for internal usage,
+// not exposed to the client, for internal usage
+type ObjectInternal struct {
+	Organization string `json:",omitempty" example:"nobl9-dev"`
+	ManifestSrc  string `json:",omitempty" example:"x.yml"`
+	OktaClientID string `json:"-"` // used only by kind Agent
+}
+
+// Metadata represents part of object which is common for all available Objects, for internal usage
+type Metadata struct {
+	Name        string `json:"name"`
+	DisplayName string `json:"displayName,omitempty"`
+	Project     string `json:"project,omitempty"`
+	Labels      Labels `json:"labels,omitempty"`
+}
+
+type Labels map[string][]string
+
+// MetadataHolder is an intermediate structure that can provides metadata related
+// field to other structures
+type MetadataHolder struct {
+	Metadata Metadata `json:"metadata"`
+}
+
+// ObjectHeader represents Header which is common for all available Objects
+type ObjectHeader struct {
+	APIVersion string `json:"apiVersion"`
+	Kind       string `json:"kind"`
+	MetadataHolder
+	ObjectInternal
+}
+
+// ObjectGeneric represents struct to which every Object is parsable
+// Specific types of Object have different structures as Spec
+type ObjectGeneric struct {
+	ObjectHeader
+	Spec json.RawMessage `json:"spec"`
+}
+
+type HistoricalDataRetrieval struct {
+	DefaultDuration HistoricalDataRetrievalDuration `json:"defaultDuration"`
+	MaxDuration     HistoricalDataRetrievalDuration `json:"maxDuration"`
+}
+
+type HistoricalDataRetrievalDuration struct {
+	Unit  string      `json:"unit" example:"Day"`
+	Value json.Number `json:"value" example:"30"`
+}
+
+type QueryDelayDuration struct {
+	Unit  string      `json:"unit" example:"Minute"`
+	Value json.Number `json:"value" example:"1"`
+}
+
+// EnhanceError annotates error with path of manifest source, if it exists
+// it not returns the same error as passed as argument
+func EnhanceError(o ObjectGeneric, err error) error {
+	if err != nil && o.ManifestSrc != "" {
+		err = fmt.Errorf("%s:\n%w", o.ManifestSrc, err)
+	}
+	return err
+}
+
+// Parse takes care of all Object supported by n9/v1alpha apiVersion
+func Parse(o ObjectGeneric, parsedObjects *APIObjects, onlyHeaders bool) error {
+	var allErrors []string
+	switch o.Kind {
+	case KindSLO:
+		slo, err := genericToSLO(o, onlyHeaders)
+		if err != nil {
+			allErrors = append(allErrors, err.Error())
+		}
+		parsedObjects.SLOs = append(parsedObjects.SLOs, slo)
+	case KindService:
+		service, err := genericToService(o, onlyHeaders)
+		if err != nil {
+			allErrors = append(allErrors, err.Error())
+		}
+		parsedObjects.Services = append(parsedObjects.Services, service)
+	case KindAgent:
+		agent, err := genericToAgent(o, onlyHeaders)
+		if err != nil {
+			allErrors = append(allErrors, err.Error())
+		}
+		parsedObjects.Agents = append(parsedObjects.Agents, agent)
+	case KindAlertPolicy:
+		alertPolicy, err := genericToAlertPolicy(o, onlyHeaders)
+		if err != nil {
+			allErrors = append(allErrors, err.Error())
+		}
+		parsedObjects.AlertPolicies = append(parsedObjects.AlertPolicies, alertPolicy)
+	case KindAlertMethod:
+		alertMethod, err := genericToAlertMethod(o, onlyHeaders)
+		if err != nil {
+			allErrors = append(allErrors, err.Error())
+		}
+		parsedObjects.AlertMethods = append(parsedObjects.AlertMethods, alertMethod)
+	case KindDirect:
+		direct, err := genericToDirect(o, onlyHeaders)
+		if err != nil {
+			allErrors = append(allErrors, err.Error())
+		}
+		parsedObjects.Directs = append(parsedObjects.Directs, direct)
+	case KindDataExport:
+		dataExport, err := genericToDataExport(o, onlyHeaders)
+		if err != nil {
+			allErrors = append(allErrors, err.Error())
+		}
+		parsedObjects.DataExports = append(parsedObjects.DataExports, dataExport)
+	case KindProject:
+		project, err := genericToProject(o, onlyHeaders)
+		if err != nil {
+			allErrors = append(allErrors, err.Error())
+		}
+		parsedObjects.Projects = append(parsedObjects.Projects, project)
+	case KindRoleBinding:
+		roleBinding, err := genericToRoleBinding(o)
+		if err != nil {
+			allErrors = append(allErrors, err.Error())
+		}
+		parsedObjects.RoleBindings = append(parsedObjects.RoleBindings, roleBinding)
+	// catching invalid kinds of objects for this apiVersion
+	default:
+		err := UnsupportedKindErr(o)
+		allErrors = append(allErrors, err.Error())
+	}
+	if len(allErrors) > 0 {
+		return errors.New(strings.Join(allErrors, "\n"))
+	}
+	return nil
+}
+
+// AnomalyConfig represents relationship between anomaly type and selected notification methods.
+type AnomalyConfig struct {
+	NoData *AnomalyConfigNoData `json:"noData" validate:"omitempty"`
+}
+
+// AnomalyConfigNoData contains alertMethods used for No Data anomaly type.
+type AnomalyConfigNoData struct {
+	AlertMethods []AnomalyConfigAlertMethod `json:"alertMethods" validate:"required"`
+}
+
+// AnomalyConfigAlertMethod represents a single alert method used in AnomalyConfig
+// defined by name and project.
+type AnomalyConfigAlertMethod struct {
+	Name    string `json:"name" validate:"required,objectName" example:"slack-monitoring-channel"`
+	Project string `json:"project,omitempty" validate:"objectName" example:"default"`
+}
