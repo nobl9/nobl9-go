a2731b90abfc97e7fda17459288c7f2ffd6ed98f
Aleksander Tarraro <aleksander.tarraro@nobl9.com>
2023-07-25 14:41:15 +0200

    Add ThousandEyes minimum agent versions

--- manifest/v1alpha/thousandeyes.go
+++ manifest/v1alpha/thousandeyes.go
@@ -12,3 +12,41 @@ const (
 	ThousandEyesDNSServerResolutionTime = "dns-server-resolution-time"
 	ThousandEyesDNSSECValid             = "dns-dnssec-valid"
 )
+
+//nolint:gochecknoglobals
+var ThousandEyesTestAgentConfig map[string]thousandEyesConfig
+
+type thousandEyesConfig struct {
+	MinimumAgent      string
+	SupportedChannels []string
+}
+
+const (
+	stable = "stable"
+	beta   = "beta"
+	alpha  = "alpha"
+)
+
+const (
+	TestTypesIntroducedAgentVersion                 = "v0.33.0"
+	AvailabilityAndThroughputIntroducedAgentVersion = "v0.52.0"
+	DNSTestTypeIntroductionAgentVersion             = "v0.67.0-beta03"
+)
+
+func init() {
+	all := []string{stable, beta, alpha}
+	betaOnly := []string{beta}
+
+	ThousandEyesTestAgentConfig = map[string]thousandEyesConfig{
+		ThousandEyesNetLatency:              {MinimumAgent: TestTypesIntroducedAgentVersion, SupportedChannels: all},
+		ThousandEyesNetLoss:                 {MinimumAgent: TestTypesIntroducedAgentVersion, SupportedChannels: all},
+		ThousandEyesWebPageLoad:             {MinimumAgent: TestTypesIntroducedAgentVersion, SupportedChannels: all},
+		ThousandEyesWebDOMLoad:              {MinimumAgent: TestTypesIntroducedAgentVersion, SupportedChannels: all},
+		ThousandEyesHTTPResponseTime:        {MinimumAgent: TestTypesIntroducedAgentVersion, SupportedChannels: all},
+		ThousandEyesServerAvailability:      {MinimumAgent: AvailabilityAndThroughputIntroducedAgentVersion, SupportedChannels: all},
+		ThousandEyesServerThroughput:        {MinimumAgent: AvailabilityAndThroughputIntroducedAgentVersion, SupportedChannels: all},
+		ThousandEyesServerTotalTime:         {MinimumAgent: DNSTestTypeIntroductionAgentVersion, SupportedChannels: betaOnly},
+		ThousandEyesDNSServerResolutionTime: {MinimumAgent: DNSTestTypeIntroductionAgentVersion, SupportedChannels: betaOnly},
+		ThousandEyesDNSSECValid:             {MinimumAgent: DNSTestTypeIntroductionAgentVersion, SupportedChannels: betaOnly},
+	}
+}
