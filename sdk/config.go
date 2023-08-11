2f3871887d48018db3f97c9dcb807cb8eb14191b
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-08-11 21:46:56 +0200

    switch to custom envconfig

--- sdk/config.go
+++ sdk/config.go
@@ -1,16 +1,16 @@
 package sdk
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"path/filepath"
+	"reflect"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/BurntSushi/toml"
 	"github.com/pkg/errors"
-	"github.com/sethvargo/go-envconfig"
 )
 
 const (
@@ -39,22 +39,22 @@ type Config struct {
 
 // GlobalConfig stores config not tied to any specific context.
 type GlobalConfig struct {
-	DefaultContext       string `toml:"defaultContext" env:"DEFAULT_CONTEXT, overwrite, default=default"`
-	FilesPromptThreshold *int   `toml:"filesPromptThreshold,omitempty" env:"FILES_PROMPT_THRESHOLD, overwrite, default=23"`
-	FilesPromptEnabled   *bool  `toml:"filesPromptEnabled,omitempty" env:"FILES_PROMPT_ENABLED, overwrite, default=true"`
+	DefaultContext       string `toml:"defaultContext" env:"DEFAULT_CONTEXT"`
+	FilesPromptThreshold *int   `toml:"filesPromptThreshold,omitempty" env:"FILES_PROMPT_THRESHOLD"`
+	FilesPromptEnabled   *bool  `toml:"filesPromptEnabled,omitempty" env:"FILES_PROMPT_ENABLED"`
 }
 
 // ContextConfig stores context specific config.
 type ContextConfig struct {
-	ClientID       string         `toml:"clientId" env:"CLIENT_ID, overwrite"`
-	ClientSecret   string         `toml:"clientSecret" env:"CLIENT_SECRET, overwrite"`
-	AccessToken    string         `toml:"accessToken,omitempty" env:"ACCESS_TOKEN, overwrite"`
-	Project        string         `toml:"project,omitempty" env:"PROJECT, overwrite"`
-	URL            string         `toml:"url,omitempty" env:"URL, overwrite"`
-	OktaOrgURL     string         `toml:"oktaOrgURL,omitempty" env:"OKTA_ORG_URL, overwrite, default=https://accounts.nobl9.com"`
-	OktaAuthServer string         `toml:"oktaAuthServer,omitempty" env:"OKTA_AUTH_SERVER, overwrite, default=auseg9kiegWKEtJZC416"`
-	DisableOkta    *bool          `toml:"disableOkta,omitempty" env:"DISABLE_OKTA, overwrite, default=false"`
-	Timeout        *time.Duration `toml:"timeout,omitempty" env:"TIMEOUT, overwrite, default=1m"`
+	ClientID       string         `toml:"clientId" env:"CLIENT_ID"`
+	ClientSecret   string         `toml:"clientSecret" env:"CLIENT_SECRET"`
+	AccessToken    string         `toml:"accessToken,omitempty" env:"ACCESS_TOKEN"`
+	Project        string         `toml:"project,omitempty" env:"PROJECT"`
+	URL            string         `toml:"url,omitempty" env:"URL"`
+	OktaOrgURL     string         `toml:"oktaOrgURL,omitempty" env:"OKTA_ORG_URL"`
+	OktaAuthServer string         `toml:"oktaAuthServer,omitempty" env:"OKTA_AUTH_SERVER"`
+	DisableOkta    *bool          `toml:"disableOkta,omitempty" env:"DISABLE_OKTA"`
+	Timeout        *time.Duration `toml:"timeout,omitempty" env:"TIMEOUT"`
 }
 
 // fileConfig contains fully parsed config file.
@@ -64,28 +64,23 @@ type fileConfig struct {
 }
 
 // ConfigOption conveys extra configuration details for ReadConfig function.
-type ConfigOption struct {
-	filePath     *string
-	contextName  *string
-	envPrefix    *string
-	noConfigFile *bool
-}
+type ConfigOption func(conf *Config)
 
 // ConfigOptionNoConfigFile instructs Config to not try reading config.toml file at all.
 func ConfigOptionNoConfigFile() ConfigOption {
-	return ConfigOption{noConfigFile: ptr(true)}
+	return func(conf *Config) { conf.noConfigFile = true }
 }
 
 // ConfigOptionUseContext instructs Config to use the provided context name.
 // It has no effect if ConfigOptionNoConfigFile is provided.
 func ConfigOptionUseContext(context string) ConfigOption {
-	return ConfigOption{contextName: &context}
+	return func(conf *Config) { conf.currentContext = context }
 }
 
-// ConfigOptionConfigFilePath instructs Config to load config file from the provided path.
+// ConfigOptionFilePath instructs Config to load config file from the provided path.
 // It has no effect if ConfigOptionNoConfigFile is provided.
-func ConfigOptionConfigFilePath(path string) ConfigOption {
-	return ConfigOption{filePath: &path}
+func ConfigOptionFilePath(path string) ConfigOption {
+	return func(conf *Config) { conf.filePath = path }
 }
 
 // ConfigOptionEnvPrefix instructs Config to lookup environment variables with the provided prefix.
@@ -93,7 +88,7 @@ func ConfigOptionConfigFilePath(path string) ConfigOption {
 //
 //	ConfigOptionEnvPrefix("SLOCTL_") --> looks up SLOCTL_CLIENT_ID env and assigns it to Config.ClientID
 func ConfigOptionEnvPrefix(prefix string) ConfigOption {
-	return ConfigOption{envPrefix: &prefix}
+	return func(conf *Config) { conf.envPrefix = prefix }
 }
 
 var (
@@ -108,7 +103,7 @@ Either set them in configuration file or provide them through env variables.
 )
 
 // ReadConfig TODO
-func ReadConfig(ctx context.Context, options ...ConfigOption) (*Config, error) {
+func ReadConfig(options ...ConfigOption) (*Config, error) {
 	conf := newConfig(options)
 	// Load both file and env configs.
 	fileConfLoaded := false
@@ -122,9 +117,11 @@ func ReadConfig(ctx context.Context, options ...ConfigOption) (*Config, error) {
 		}
 	}
 	// Read global settings from env variables.
-	if err := envconfig.Process(ctx, &conf.GlobalConfig); err != nil {
-		return nil, errors.Wrap(err, "failed to process env variables configuration")
+	if err := conf.processEnvVariables(&conf.GlobalConfig); err != nil {
+		return nil, err
 	}
+	// Only once we've read both file and env configuration can we resolve the context.
+	conf.setCurrentContext()
 	// Once we know the context to operate on, we can try choosing the right context from file config.
 	if fileConfLoaded {
 		var ok bool
@@ -135,8 +132,8 @@ func ReadConfig(ctx context.Context, options ...ConfigOption) (*Config, error) {
 		}
 	}
 	// Finally read the rest of env variables and overwrite values.
-	if err := envconfig.Process(ctx, &conf.ContextConfig); err != nil {
-		return nil, errors.Wrap(err, "failed to process env variables configuration")
+	if err := conf.processEnvVariables(&conf.ContextConfig); err != nil {
+		return nil, err
 	}
 	// Validate and correct.
 	conf.URL = strings.TrimRight(conf.URL, "/")
@@ -148,31 +145,7 @@ func ReadConfig(ctx context.Context, options ...ConfigOption) (*Config, error) {
 	return conf, nil
 }
 
-func newConfig(options []ConfigOption) *Config {
-	conf := &Config{
-		filePath:       defaultConfigPath,
-		currentContext: defaultContext,
-		noConfigFile:   false,
-		envPrefix:      EnvPrefix,
-	}
-	for _, opt := range options {
-		if opt.noConfigFile != nil {
-			conf.noConfigFile = *opt.noConfigFile
-		}
-		if opt.filePath != nil {
-			conf.filePath = *opt.filePath
-		}
-		if opt.contextName != nil {
-			conf.currentContext = *opt.contextName
-		}
-		if opt.envPrefix != nil {
-			conf.envPrefix = *opt.envPrefix
-		}
-	}
-	return conf
-}
-
-func (c *Config) SaveConfigFile() error {
+func (c *Config) Save() error {
 	var err error
 	if c.filePath == "" {
 		return errors.New("config file path must be provided")
@@ -209,6 +182,27 @@ func (c *Config) SaveConfigFile() error {
 		return err
 	}
 	return os.Rename(tmpFile.Name(), c.filePath)
+}
+
+func newConfig(options []ConfigOption) *Config {
+	conf := &Config{
+		filePath:     getDefaultConfigPath(),
+		noConfigFile: false,
+		envPrefix:    EnvPrefix,
+	}
+	for _, applyOption := range options {
+		applyOption(conf)
+	}
+	return conf
+}
+
+func (c *Config) setCurrentContext() {
+	// Context override from ConfigOption takes precedence.
+	if c.currentContext != "" {
+		return
+	}
+	// Set from env/file configuration.
+	c.currentContext = c.GlobalConfig.DefaultContext
 }
 
 func (c *Config) loadConfigFile() error {
@@ -255,7 +249,147 @@ func (c *Config) createDefaultConfig() error {
 	return nil
 }
 
-var defaultConfigPath = getDefaultConfigPath()
+var configDefaults = map[string]string{
+	"DEFAULT_CONTEXT":        "default",
+	"FILES_PROMPT_THRESHOLD": "23",
+	"FILES_PROMPT_ENABLED":   "true",
+	"OKTA_ORG_URL":           defaultOktaOrgURL,
+	"OKTA_AUTH_SERVER":       defaultOktaAuthServerID,
+	"DISABLE_OKTA":           "false",
+	"TIMEOUT":                "1m",
+}
+
+// processEnvVariables takes a struct pointer and scans its fields tags looking for "env"
+// tag which should contain the environment variable name of the given struct field.
+// Example:
+func (c *Config) processEnvVariables(iv interface{}) error {
+	v := reflect.ValueOf(iv)
+	if v.Kind() != reflect.Ptr {
+		return errors.New("input must be a pointer")
+	}
+	e := v.Elem()
+	if e.Kind() != reflect.Struct {
+		return errors.New("input must be a struct")
+	}
+	t := e.Type()
+
+	for i := 0; i < t.NumField(); i++ {
+		ef := e.Field(i)
+		tf := t.Field(i)
+		key := tf.Tag.Get("env")
+
+		if !ef.CanSet() {
+			if key != "" {
+				return fmt.Errorf("%s: %w", tf.Name, errors.New("cannot parse private field"))
+			}
+			continue
+		}
+
+		// If struct, drill deeper.
+		if ef.Kind() == reflect.Struct {
+			for ef.CanAddr() {
+				ef = ef.Addr()
+			}
+			if err := c.processEnvVariables(ef.Interface()); err != nil {
+				return fmt.Errorf("%s: %w", tf.Name, err)
+			}
+			continue
+		}
+		// Only now check if the key has been provided.
+		if key == "" {
+			continue
+		}
+
+		val, found := os.LookupEnv(c.envPrefix + key)
+		// If the field already has a non-zero value and there was no value directly
+		// specified, do not overwrite the existing field. We only want to overwrite
+		// when the env var was provided directly.
+		if !ef.IsZero() && !found {
+			continue
+		}
+
+		// Check for default value.
+		if val == "" {
+			var hasDefault bool
+			val, hasDefault = configDefaults[key]
+			// If the value is empty and we don't have a default, don't do anything.
+			if !hasDefault {
+				continue
+			}
+		}
+
+		// Set value.
+		if err := c.setConfigFieldValue(val, ef); err != nil {
+			return fmt.Errorf("%s(%q): %w", tf.Name, val, err)
+		}
+	}
+
+	return nil
+}
+
+// setConfigFieldValue sets the value of the Config field using reflection.
+func (c *Config) setConfigFieldValue(v string, ef reflect.Value) error {
+	if v == "" {
+		return nil
+	}
+
+	// Handle pointers and uninitialized pointers.
+	for ef.Type().Kind() == reflect.Ptr {
+		if ef.IsNil() {
+			ef.Set(reflect.New(ef.Type().Elem()))
+		}
+		ef = ef.Elem()
+	}
+
+	tf := ef.Type()
+	tk := tf.Kind()
+
+	switch tk {
+	case reflect.Bool:
+		b, err := strconv.ParseBool(v)
+		if err != nil {
+			return err
+		}
+		ef.SetBool(b)
+	case reflect.Float32, reflect.Float64:
+		f, err := strconv.ParseFloat(v, tf.Bits())
+		if err != nil {
+			return err
+		}
+		ef.SetFloat(f)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32:
+		i, err := strconv.ParseInt(v, 0, tf.Bits())
+		if err != nil {
+			return err
+		}
+		ef.SetInt(i)
+	case reflect.Int64:
+		// Special case time.Duration values.
+		if tf.PkgPath() == "time" && tf.Name() == "Duration" {
+			d, err := time.ParseDuration(v)
+			if err != nil {
+				return err
+			}
+			ef.SetInt(int64(d))
+		} else {
+			i, err := strconv.ParseInt(v, 0, tf.Bits())
+			if err != nil {
+				return err
+			}
+			ef.SetInt(i)
+		}
+	case reflect.String:
+		ef.SetString(v)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		i, err := strconv.ParseUint(v, 0, tf.Bits())
+		if err != nil {
+			return err
+		}
+		ef.SetUint(i)
+	}
+
+	return nil
+}
 
 func getDefaultConfigPath() string {
 	home, err := os.UserHomeDir()
@@ -265,5 +399,3 @@ func getDefaultConfigPath() string {
 	}
 	return filepath.Clean(filepath.Join(home, defaultRelativeConfigPath))
 }
-
-func ptr[T any](v T) *T { return &v }
--- sdk/config_test.go
+++ sdk/config_test.go
@@ -1,14 +1,15 @@
 package sdk
 
 import (
-	"context"
 	"embed"
 	_ "embed"
 	"io"
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -17,33 +18,141 @@ var configTestData embed.FS
 
 const configTestDataPath = "test_data/config"
 
-func TestReadConfig_FromConfigFile(t *testing.T) {
-	setupConfigTestData(t)
+func TestReadConfig_FromMinimalConfigFile(t *testing.T) {
+	tempDir := setupConfigTestData(t)
+	expected := &Config{
+		GlobalConfig: GlobalConfig{
+			DefaultContext:       "my-context",
+			FilesPromptEnabled:   ptr(true),
+			FilesPromptThreshold: ptr(23),
+		},
+		ContextConfig: ContextConfig{
+			ClientID:       "someId",
+			ClientSecret:   "someSecret",
+			OktaOrgURL:     defaultOktaOrgURL,
+			OktaAuthServer: defaultOktaAuthServerID,
+			DisableOkta:    ptr(false),
+			Timeout:        ptr(time.Minute),
+		},
+		fileConfig: fileConfig{
+			GlobalConfig: GlobalConfig{
+				DefaultContext: "my-context",
+			},
+			Contexts: map[string]ContextConfig{
+				"my-context": {
+					ClientID:     "someId",
+					ClientSecret: "someSecret",
+				},
+			},
+		},
+		currentContext: "my-context",
+		noConfigFile:   false,
+		envPrefix:      EnvPrefix,
+	}
 
-	ReadConfig(context.Background())
+	t.Run("custom config file", func(t *testing.T) {
+		filePath := filepath.Join(tempDir, "minimal_config.toml")
+
+		conf, err := ReadConfig(ConfigOptionFilePath(filePath))
+		require.NoError(t, err)
+
+		expected.filePath = filePath
+		assert.EqualValues(t, expected, conf)
+	})
+
+	t.Run("default config file", func(t *testing.T) {
+		t.Setenv("HOME", tempDir)
+		filePath := filepath.Join(tempDir, defaultRelativeConfigPath)
+		copyEmbeddedFile(t, "minimal_config.toml", filePath)
+
+		conf, err := ReadConfig(ConfigOptionFilePath(filePath))
+		require.NoError(t, err)
+
+		expected.filePath = filePath
+		assert.EqualValues(t, expected, conf)
+	})
+}
+
+func TestReadConfig_FromFullConfigFile(t *testing.T) {
+	tempDir := setupConfigTestData(t)
+	filePath := filepath.Join(tempDir, "full_config.toml")
+
+	conf, err := ReadConfig(ConfigOptionFilePath(filePath))
 	require.NoError(t, err)
+
+	assert.EqualValues(t, &Config{
+		GlobalConfig: GlobalConfig{
+			DefaultContext:       "non-default",
+			FilesPromptEnabled:   ptr(false),
+			FilesPromptThreshold: ptr(30),
+		},
+		ContextConfig: ContextConfig{
+			ClientID:       "non-default-client-id",
+			ClientSecret:   "non-default-client-secret",
+			AccessToken:    "non-default-access-token",
+			Project:        "non-default-project",
+			OktaOrgURL:     "https://non-default-okta-org-url.com",
+			OktaAuthServer: "non-default-okta-auth-server",
+			Timeout:        ptr(100 * time.Minute),
+			URL:            "https://non-default-url.com",
+			DisableOkta:    ptr(true),
+		},
+		fileConfig: fileConfig{
+			GlobalConfig: GlobalConfig{
+				DefaultContext:       "non-default",
+				FilesPromptEnabled:   ptr(false),
+				FilesPromptThreshold: ptr(30),
+			},
+			Contexts: map[string]ContextConfig{
+				"default": {
+					ClientID:     "default-client-id",
+					ClientSecret: "default-client-secret",
+				},
+				"non-default": {
+					ClientID:       "non-default-client-id",
+					ClientSecret:   "non-default-client-secret",
+					AccessToken:    "non-default-access-token",
+					Project:        "non-default-project",
+					OktaOrgURL:     "https://non-default-okta-org-url.com",
+					OktaAuthServer: "non-default-okta-auth-server",
+					Timeout:        ptr(100 * time.Minute),
+					URL:            "https://non-default-url.com",
+					DisableOkta:    ptr(true),
+				},
+			},
+		},
+		filePath:       filePath,
+		currentContext: "non-default",
+		noConfigFile:   false,
+		envPrefix:      EnvPrefix,
+	}, conf)
 }
 
 func setupConfigTestData(t *testing.T) (tempDir string) {
 	t.Helper()
-	tempDir = os.TempDir()
+	tempDir = t.TempDir()
 	dirEntries, err := configTestData.ReadDir(configTestDataPath)
 	require.NoError(t, err)
 	for _, entry := range dirEntries {
-		copyEmbeddedFileToTempFile(t, tempDir, entry.Name())
+		copyEmbeddedFile(t, entry.Name(), filepath.Join(tempDir, entry.Name()))
 	}
 	return tempDir
 }
 
-func copyEmbeddedFileToTempFile(t *testing.T, tmpDir string, filename string) {
-	embeddedFile, err := configTestData.Open(filepath.Join(configTestDataPath, filename))
+func copyEmbeddedFile(t *testing.T, sourceName string, dest string) {
+	embeddedFile, err := configTestData.Open(filepath.Join(configTestDataPath, sourceName))
 	require.NoError(t, err)
 	defer func() { _ = embeddedFile.Close() }()
 
-	tmpFile, err := os.Create(filepath.Join(tmpDir, filename))
+	err = os.MkdirAll(filepath.Dir(dest), 0770)
+	require.NoError(t, err)
+
+	tmpFile, err := os.Create(dest)
 	require.NoError(t, err)
 	defer func() { _ = tmpFile.Close() }()
 
 	_, err = io.Copy(tmpFile, embeddedFile)
 	require.NoError(t, err)
 }
+
+func ptr[T any](v T) *T { return &v }
