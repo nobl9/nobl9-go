12338b580205c8623c9d81dea6e5704efc101576
Adam Szymanski <adam.szymanski@nobl9.com>
2023-08-16 14:27:39 +0200

    Move tests

--- manifest/v1alpha/validator_test.go
+++ manifest/v1alpha/validator_test.go
@@ -1016,3 +1016,39 @@ func TestIsReleaseChannelValid(t *testing.T) {
 		})
 	}
 }
+
+func TestAlertingWindowValidation(t *testing.T) {
+	for testCase, isValid := range map[string]bool{
+		"5m":             true,
+		"1h":             true,
+		"72h":            true,
+		"1h30m":          true,
+		"1h1m60s":        true,
+		"300s":           true,
+		"0.1h":           true,
+		"300000ms":       true,
+		"300000000000ns": true,
+		"30000000000ns":  false,
+		"3m":             false,
+		"120s":           false,
+		"5m30s":          false,
+		"1h5m5s":         false,
+		"0.01h":          false,
+		"555s":           false,
+	} {
+		condition := AlertCondition{
+			Measurement:    MeasurementAverageBurnRate.String(),
+			Value:          1.0,
+			AlertingWindow: testCase,
+		}
+
+		t.Run(testCase, func(t *testing.T) {
+			err := NewValidator().Check(condition)
+			if isValid {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
