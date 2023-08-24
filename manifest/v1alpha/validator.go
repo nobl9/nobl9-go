97d81db442e257fe4593b061ff64b748cc94f6d9
Mateusz Hawrus <48822818+nieomylnieja@users.noreply.github.com>
2023-08-24 12:44:11 +0200

    move validator related code to validator.go (#109)

--- manifest/v1alpha/validator.go
+++ manifest/v1alpha/validator.go
@@ -483,6 +483,18 @@ func sloSpecStructLevelValidation(sl v.StructLevel) {
 	sloSpecStructLevelAnomalyConfigValidation(sl, sloSpec)
 }
 
+func isBurnRateSetForCompositeWithOccurrences(spec SLOSpec) bool {
+	return !isBudgetingMethodOccurrences(spec) || spec.Composite.BurnRateCondition != nil
+}
+
+func isValidBudgetingMethodForCompositeWithBurnRate(spec SLOSpec) bool {
+	return spec.Composite.BurnRateCondition == nil || isBudgetingMethodOccurrences(spec)
+}
+
+func isBudgetingMethodOccurrences(sloSpec SLOSpec) bool {
+	return sloSpec.BudgetingMethod == BudgetingMethodOccurrences.String()
+}
+
 func sloSpecStructLevelAppDynamicsValidation(sl v.StructLevel, sloSpec SLOSpec) {
 	if !haveCountMetricsTheSameAppDynamicsApplicationNames(sloSpec) {
 		sl.ReportError(
