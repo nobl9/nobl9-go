e21e5f3dbd0512f7cd960665fd27e1f1e4fd6182
Jakub Miklasz <jakub.miklasz@nobl9.com>
2023-09-11 13:01:37 +0200

    add validation

--- manifest/v1alpha/agent.go
+++ manifest/v1alpha/agent.go
@@ -235,7 +235,7 @@ type SplunkAgentConfig struct {
 
 // AzureMonitorAgentConfig represents content of AzureMonitor Configuration typical for Agent Object.
 type AzureMonitorAgentConfig struct {
-	TenantID string `json:"tenantId" example:"abf988bf-86f1-41af-91ab-2d7cd011db46"`
+	TenantID string `json:"tenantId" validate:"required" example:"abf988bf-86f1-41af-91ab-2d7cd011db46"`
 }
 
 // AgentWithSLOs struct which mapped one to one with kind: agent and slo yaml definition
