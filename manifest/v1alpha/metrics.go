61fe1666dcdefc1f17c8ced0d19acf250334bef9
Jakub Miklasz <jakub.miklasz@nobl9.com>
2023-09-15 12:28:51 +0200

    remove nolint

--- manifest/v1alpha/metrics.go
+++ manifest/v1alpha/metrics.go
@@ -227,7 +227,7 @@ type GrafanaLokiMetric struct {
 type AzureMonitorMetric struct {
 	ResourceID      string                        `json:"resourceId" validate:"required"`
 	MetricName      string                        `json:"metricName" validate:"required"`
-	Aggregation     string                        `json:"aggregation" validate:"required"` //nolint:lll
+	Aggregation     string                        `json:"aggregation" validate:"required"`
 	Dimensions      []AzureMonitorMetricDimension `json:"dimensions,omitempty" validate:"uniqueDimensionNames,dive"`
 	MetricNamespace string                        `json:"metricNamespace,omitempty"`
 }
