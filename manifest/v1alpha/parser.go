836af2e4019b8b52fadfabfd5fbee999e8742a23
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-09-15 17:39:55 +0200

    turn on strict mode

--- manifest/v1alpha/parser.go
+++ manifest/v1alpha/parser.go
@@ -1,6 +1,7 @@
 package v1alpha
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 
@@ -10,13 +11,17 @@ import (
 	"github.com/nobl9/nobl9-go/manifest"
 )
 
-type unmarshalFunc func(data []byte, v interface{}) error
+type unmarshalFunc func(v interface{}) error
 
 func ParseObject(data []byte, kind manifest.Kind, format manifest.ObjectFormat) (manifest.Object, error) {
 	var unmarshal unmarshalFunc
 	switch format {
 	case manifest.ObjectFormatJSON:
-		unmarshal = json.Unmarshal
+		unmarshal = func(v interface{}) error {
+			dec := json.NewDecoder(bytes.NewReader(data))
+			dec.DisallowUnknownFields()
+			return dec.Decode(v)
+		}
 	case manifest.ObjectFormatYAML:
 		// Workaround for https://github.com/goccy/go-yaml/issues/313.
 		// If the library changes its interpretation of empty pointer fields,
@@ -26,7 +31,9 @@ func ParseObject(data []byte, kind manifest.Kind, format manifest.ObjectFormat) 
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to convert YAML to JSON")
 		}
-		unmarshal = json.Unmarshal
+		unmarshal = func(v interface{}) error {
+			return yaml.UnmarshalWithOptions(data, v, yaml.Strict())
+		}
 	default:
 		return nil, errors.Errorf("unsupported format: %s", format)
 	}
@@ -38,31 +45,31 @@ func ParseObject(data []byte, kind manifest.Kind, format manifest.ObjectFormat) 
 	//exhaustive:enforce
 	switch kind {
 	case manifest.KindService:
-		object, err = genericParseObject[Service](data, unmarshal)
+		object, err = genericParseObject[Service](unmarshal)
 	case manifest.KindSLO:
-		object, err = genericParseObject[SLO](data, unmarshal)
+		object, err = genericParseObject[SLO](unmarshal)
 	case manifest.KindProject:
-		object, err = genericParseObject[Project](data, unmarshal)
+		object, err = genericParseObject[Project](unmarshal)
 	case manifest.KindAgent:
-		object, err = genericParseObject[Agent](data, unmarshal)
+		object, err = genericParseObject[Agent](unmarshal)
 	case manifest.KindDirect:
-		object, err = genericParseObject[Direct](data, unmarshal)
+		object, err = genericParseObject[Direct](unmarshal)
 	case manifest.KindAlert:
-		object, err = genericParseObject[Alert](data, unmarshal)
+		object, err = genericParseObject[Alert](unmarshal)
 	case manifest.KindAlertMethod:
-		object, err = genericParseObject[AlertMethod](data, unmarshal)
+		object, err = genericParseObject[AlertMethod](unmarshal)
 	case manifest.KindAlertPolicy:
-		object, err = genericParseObject[AlertPolicy](data, unmarshal)
+		object, err = genericParseObject[AlertPolicy](unmarshal)
 	case manifest.KindAlertSilence:
-		object, err = genericParseObject[AlertSilence](data, unmarshal)
+		object, err = genericParseObject[AlertSilence](unmarshal)
 	case manifest.KindRoleBinding:
-		object, err = genericParseObject[RoleBinding](data, unmarshal)
+		object, err = genericParseObject[RoleBinding](unmarshal)
 	case manifest.KindDataExport:
-		object, err = genericParseObject[DataExport](data, unmarshal)
+		object, err = genericParseObject[DataExport](unmarshal)
 	case manifest.KindAnnotation:
-		object, err = genericParseObject[Annotation](data, unmarshal)
+		object, err = genericParseObject[Annotation](unmarshal)
 	case manifest.KindUserGroup:
-		object, err = genericParseObject[UserGroup](data, unmarshal)
+		object, err = genericParseObject[UserGroup](unmarshal)
 	default:
 		return nil, fmt.Errorf("%s is %w", kind, manifest.ErrInvalidKind)
 	}
@@ -72,9 +79,9 @@ func ParseObject(data []byte, kind manifest.Kind, format manifest.ObjectFormat) 
 	return object, nil
 }
 
-func genericParseObject[T manifest.Object](data []byte, unmarshal unmarshalFunc) (T, error) {
+func genericParseObject[T manifest.Object](unmarshal unmarshalFunc) (T, error) {
 	var object T
-	if err := unmarshal(data, &object); err != nil {
+	if err := unmarshal(&object); err != nil {
 		return object, err
 	}
 	return object, nil
