fd08be1d19f5b2e411ee47d0714f97e1bfc73bd7
Mateusz Hawrus <48822818+nieomylnieja@users.noreply.github.com>
2023-09-22 16:06:52 +0200

    PC-10369: Correct GenericObject handling (#126)
    * correct generic object handling
    
    * correct field name

--- manifest/v1alpha/generic_object.go
+++ manifest/v1alpha/generic_object.go
@@ -4,25 +4,38 @@ import (
 	"github.com/nobl9/nobl9-go/manifest"
 )
 
+const (
+	genericFieldVersion        = "apiVersion"
+	genericFieldKind           = "kind"
+	genericFieldMetadata       = "metadata"
+	genericFieldName           = "name"
+	genericFieldProject        = "project"
+	genericFieldOrganization   = "organization"
+	genericFieldManifestSource = "manifestSrc"
+)
+
 // GenericObject represents a generic map[string]interface{} representation of manifest.Object.
 // It's useful for scenarios where an implementation does not want to be tied to specific
 // v1alpha versions.
 type GenericObject map[string]interface{}
 
 func (g GenericObject) GetVersion() string {
-	version, _ := g["apiVersion"].(string)
+	version, _ := g[genericFieldVersion].(string)
 	return version
 }
 
 func (g GenericObject) GetKind() manifest.Kind {
-	name, _ := g["kind"].(string)
+	name, _ := g[genericFieldKind].(string)
 	kind, _ := manifest.ParseKind(name)
 	return kind
 }
 
 func (g GenericObject) GetName() string {
-	meta, _ := g["metadata"].(map[string]interface{})
-	name, _ := meta["name"].(string)
+	meta, ok := g[genericFieldMetadata].(map[string]interface{})
+	if !ok {
+		return ""
+	}
+	name, _ := meta[genericFieldName].(string)
 	return name
 }
 
@@ -31,34 +44,45 @@ func (g GenericObject) Validate() error {
 }
 
 func (g GenericObject) GetProject() string {
-	meta, _ := g["metadata"].(map[string]interface{})
-	name, _ := meta["project"].(string)
+	meta, ok := g[genericFieldMetadata].(map[string]interface{})
+	if !ok {
+		return ""
+	}
+	name, _ := meta[genericFieldProject].(string)
 	return name
 }
 
 func (g GenericObject) SetProject(project string) manifest.Object {
-	meta, _ := g["metadata"].(map[string]interface{})
-	meta["project"] = project
-	g["metadata"] = meta
-	return g
+	switch g.GetKind() {
+	case 0, manifest.KindProject, manifest.KindRoleBinding, manifest.KindUserGroup:
+		return g
+	default:
+		meta, ok := g[genericFieldMetadata].(map[string]interface{})
+		if !ok {
+			return g
+		}
+		meta[genericFieldProject] = project
+		g[genericFieldMetadata] = meta
+		return g
+	}
 }
 
 func (g GenericObject) GetOrganization() string {
-	org, _ := g["organization"].(string)
+	org, _ := g[genericFieldOrganization].(string)
 	return org
 }
 
 func (g GenericObject) SetOrganization(org string) manifest.Object {
-	g["organization"] = org
+	g[genericFieldOrganization] = org
 	return g
 }
 
 func (g GenericObject) GetManifestSource() string {
-	src, _ := g["manifestSrc"].(string)
+	src, _ := g[genericFieldManifestSource].(string)
 	return src
 }
 
 func (g GenericObject) SetManifestSource(src string) manifest.Object {
-	g["manifestSrc"] = src
+	g[genericFieldManifestSource] = src
 	return g
 }
