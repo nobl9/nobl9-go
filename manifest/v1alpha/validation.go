c13cde984ad6f5d1b58adbc1fbefa4da3b2b7bd5
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-10-04 15:13:20 +0200

    use common validators

--- manifest/v1alpha/validation.go
+++ manifest/v1alpha/validation.go
@@ -2,27 +2,27 @@ package v1alpha
 
 import "github.com/nobl9/nobl9-go/validation"
 
-func FieldRuleMetdataName(getter func() string) validation.FieldRules[string] {
+func FieldRuleMetadataName[S any](getter func(S) string) validation.FieldRules[string, S] {
 	return validation.RulesForField[string]("metadata.name", getter).
 		With(validation.StringRequired(), validation.StringIsDNSSubdomain())
 }
 
-func FieldRuleMetadataDisplayName(getter func() string) validation.FieldRules[string] {
+func FieldRuleMetadataDisplayName[S any](getter func(S) string) validation.FieldRules[string, S] {
 	return validation.RulesForField[string]("metadata.displayName", getter).
 		With(validation.StringLength(0, 63))
 }
 
-func FieldRuleMetdataProject(getter func() string) validation.FieldRules[string] {
+func FieldRuleMetadataProject[S any](getter func(S) string) validation.FieldRules[string, S] {
 	return validation.RulesForField[string]("metadata.project", getter).
 		With(validation.StringRequired(), validation.StringIsDNSSubdomain())
 }
 
-func FieldRuleMetadataLabels(getter func() Labels) validation.FieldRules[Labels] {
+func FieldRuleMetadataLabels[S any](getter func(S) Labels) validation.FieldRules[Labels, S] {
 	return validation.RulesForField[Labels]("metadata.labels", getter).
-		With(ValidationRule())
+		With(ValidationRuleLabels())
 }
 
-func FieldRuleMetadataDescription(getter func() string) validation.FieldRules[string] {
+func FieldRuleSpecDescription[S any](getter func(S) string) validation.FieldRules[string, S] {
 	return validation.RulesForField[string]("spec.description", getter).
 		With(validation.StringDescription())
 }
