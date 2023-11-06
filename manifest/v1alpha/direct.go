820684bc2c76740992d9d1fd1f86efcdf6cca6a6
kubaceg <jakub.cegielka@nobl9.com>
2023-11-06 15:00:37 +0100

    PC-10566 Terraform nobl9_cloudwatch/redshift_direct resource should return external id.

--- manifest/v1alpha/direct.go
+++ manifest/v1alpha/direct.go
@@ -323,9 +323,10 @@ type CloudWatchDirectConfig struct {
 // PublicCloudWatchDirectConfig represents content of CloudWatch Configuration typical for Direct Object
 // without secrets.
 type PublicCloudWatchDirectConfig struct {
-	HiddenAccessKeyID     string `json:"accessKeyID,omitempty"`
-	HiddenSecretAccessKey string `json:"secretAccessKey,omitempty"`
-	HiddenRoleARN         string `json:"roleARN,omitempty"`
+	HiddenAccessKeyID     string                     `json:"accessKeyID,omitempty"`
+	HiddenSecretAccessKey string                     `json:"secretAccessKey,omitempty"`
+	HiddenRoleARN         string                     `json:"roleARN,omitempty"`
+	CrossAccountIAMRoles  *AwsIAMRoleAuthExternalIDs `json:"crossAccountIAMRoles,omitempty"`
 }
 
 // PingdomDirectConfig represents content of Pingdom Configuration typical for Direct Object.
@@ -373,10 +374,11 @@ type RedshiftDirectConfig struct {
 
 // PublicRedshiftDirectConfig represents content of Redshift configuration typical for Direct Object without secrets.
 type PublicRedshiftDirectConfig struct {
-	HiddenAccessKeyID     string `json:"accessKeyID,omitempty"`
-	HiddenSecretAccessKey string `json:"secretAccessKey,omitempty"`
-	SecretARN             string `json:"secretARN"`
-	HiddenRoleARN         string `json:"roleARN,omitempty"`
+	HiddenAccessKeyID     string                     `json:"accessKeyID,omitempty"`
+	HiddenSecretAccessKey string                     `json:"secretAccessKey,omitempty"`
+	SecretARN             string                     `json:"secretARN"`
+	HiddenRoleARN         string                     `json:"roleARN,omitempty"`
+	CrossAccountIAMRoles  *AwsIAMRoleAuthExternalIDs `json:"crossAccountIAMRoles,omitempty"`
 }
 
 // SumoLogicDirectConfig represents content of SumoLogic configuration typical for Direct Object.
@@ -412,4 +414,10 @@ type PublicAzureMonitorDirectConfig struct {
 type PublicDirectWithSLOs struct {
 	Direct PublicDirect `json:"direct"`
 	SLOs   []SLO        `json:"slos"`
+}
+
+// AwsIAMRoleAuthExternalIDs struct which is used for exposing AWS IAM role auth data
+type AwsIAMRoleAuthExternalIDs struct {
+	AWSExternalID string `json:"awsExternalID"`
+	AccountID     string `json:"accountID"`
 }
