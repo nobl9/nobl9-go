b6d9efed51ac5a42eec5151305e744e2d20bbfd2
bsski <swedrowskibartosz@gmail.com>
2023-11-14 14:27:09 +0100

    PC-10654 Lint fix

--- manifest/v1alpha/metrics.go
+++ manifest/v1alpha/metrics.go
@@ -261,7 +261,7 @@ type GenericMetric struct {
 // HoneycombMetric represents metric from Honeycomb.
 type HoneycombMetric struct {
 	Dataset     string          `json:"dataset" validate:"required,max=255,ascii,notBlank"`
-	Calculation string          `json:"calculation" validate:"required,max=30,ascii,notBlank,supportedHoneycombCalculationType"`
+	Calculation string          `json:"calculation" validate:"required,max=30,ascii,notBlank,supportedHoneycombCalculationType"` //nolint:lll
 	Attribute   string          `json:"attribute" validate:"required,max=255,ascii,notBlank"`
 	Filter      HoneycombFilter `json:"filter"`
 }
