706783b2e3943e0f560be8c09b504651b43f8bec
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-11-17 12:39:59 +0100

    correct generate path

--- manifest/v1alpha/usergroup/user_group.go
+++ manifest/v1alpha/usergroup/user_group.go
@@ -5,7 +5,7 @@ import (
 	"github.com/nobl9/nobl9-go/manifest/v1alpha"
 )
 
-//go:generate go run ../../scripts/generate-object-impl.go UserGroup
+//go:generate go run ../../../scripts/generate-object-impl.go UserGroup
 
 func New(metadata Metadata, spec Spec) UserGroup {
 	return UserGroup{
