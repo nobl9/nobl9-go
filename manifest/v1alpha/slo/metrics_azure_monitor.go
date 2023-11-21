16b01e5f9c397271163ffdd6d83356def1c0dbad
Nikodem Rafalski <nikodem.rafalski@nobl9.com>
2023-11-21 11:38:48 +0100

    Azure monitor logs analytics metrics support

--- manifest/v1alpha/slo/metrics_azure_monitor.go
+++ manifest/v1alpha/slo/metrics_azure_monitor.go
@@ -7,13 +7,21 @@ import (
 	"github.com/nobl9/nobl9-go/validation"
 )
 
+const (
+	AzureMonitorDataTypeMetrics = "metrics"
+	AzureMonitorDataTypeLogs    = "logs"
+)
+
 // AzureMonitorMetric represents metric from AzureMonitor
 type AzureMonitorMetric struct {
+	DataType        string                        `json:"dataType"`
 	ResourceID      string                        `json:"resourceId"`
 	MetricName      string                        `json:"metricName"`
 	Aggregation     string                        `json:"aggregation"`
 	Dimensions      []AzureMonitorMetricDimension `json:"dimensions,omitempty"`
 	MetricNamespace string                        `json:"metricNamespace,omitempty"`
+	KQLQuery        string                        `json:"kqlQuery"`
+	WorkspaceID     string                        `json:"workspaceId"`
 }
 
 // AzureMonitorMetricDimension represents name/value pair that is part of the identity of a metric.
@@ -21,36 +29,6 @@ type AzureMonitorMetricDimension struct {
 	Name  *string `json:"name"`
 	Value *string `json:"value"`
 }
-
-var azureMonitorCountMetricsLevelValidation = validation.New[CountMetricsSpec](
-	validation.For(validation.GetSelf[CountMetricsSpec]()).Rules(
-		validation.NewSingleRule(func(c CountMetricsSpec) error {
-			total := c.TotalMetric
-			good := c.GoodMetric
-			bad := c.BadMetric
-
-			if total == nil {
-				return nil
-			}
-			if good != nil {
-				if good.AzureMonitor.MetricNamespace != total.AzureMonitor.MetricNamespace {
-					return countMetricsPropertyEqualityError("azureMonitor.metricNamespace", goodMetric)
-				}
-				if good.AzureMonitor.ResourceID != total.AzureMonitor.ResourceID {
-					return countMetricsPropertyEqualityError("azureMonitor.resourceId", goodMetric)
-				}
-			}
-			if bad != nil {
-				if bad.AzureMonitor.MetricNamespace != total.AzureMonitor.MetricNamespace {
-					return countMetricsPropertyEqualityError("azureMonitor.metricNamespace", badMetric)
-				}
-				if bad.AzureMonitor.ResourceID != total.AzureMonitor.ResourceID {
-					return countMetricsPropertyEqualityError("azureMonitor.resourceId", badMetric)
-				}
-			}
-			return nil
-		}).WithErrorCode(validation.ErrorCodeNotEqualTo)),
-).When(whenCountMetricsIs(v1alpha.AzureMonitor))
 
 var supportedAzureMonitorAggregations = []string{
 	"Avg",
@@ -60,9 +38,30 @@ var supportedAzureMonitorAggregations = []string{
 	"Sum",
 }
 
+var supportedAzureMonitorDataTypes = []string{
+	AzureMonitorDataTypeMetrics,
+	AzureMonitorDataTypeLogs,
+}
+
+func azureMonitorMetricDataTypeIs(dataType string) validation.Predicate[AzureMonitorMetric] {
+	return func(m AzureMonitorMetric) bool {
+		return m.DataType == dataType
+	}
+}
+
 var azureMonitorResourceIDRegexp = regexp.MustCompile(`^\/subscriptions\/[a-zA-Z0-9-]+\/resourceGroups\/[a-zA-Z0-9-._()]+\/providers\/[a-zA-Z0-9-.()_]+\/[a-zA-Z0-9-_()]+\/[a-zA-Z0-9-_()]+$`) //nolint:lll
 
 var azureMonitorValidation = validation.New[AzureMonitorMetric](
+	validation.For(validation.GetSelf[AzureMonitorMetric]()).
+		Include(azureMonitorMetricDataTypeValidation).
+		Include(azureMonitorMetricLogsDataTypeValidation),
+	validation.For(func(p AzureMonitorMetric) string { return p.DataType }).
+		WithName("dataType").
+		Required().
+		Rules(validation.OneOf(supportedAzureMonitorDataTypes...)),
+)
+
+var azureMonitorMetricDataTypeValidation = validation.New[AzureMonitorMetric](
 	validation.For(func(a AzureMonitorMetric) string { return a.MetricName }).
 		WithName("metricName").
 		Required(),
@@ -85,7 +84,16 @@ var azureMonitorValidation = validation.New[AzureMonitorMetric](
 			}
 			return *d.Name
 		}).WithDetails("dimension 'name' must be unique for all dimensions")),
-)
+).When(azureMonitorMetricDataTypeIs(AzureMonitorDataTypeMetrics))
+
+var azureMonitorMetricLogsDataTypeValidation = validation.New[AzureMonitorMetric](
+	validation.For(func(a AzureMonitorMetric) string { return a.KQLQuery }).
+		WithName("kqlQuery").
+		Required(),
+	validation.For(func(a AzureMonitorMetric) string { return a.WorkspaceID }).
+		WithName("workspaceId").
+		Required(),
+).When(azureMonitorMetricDataTypeIs(AzureMonitorDataTypeLogs))
 
 var azureMonitorMetricDimensionValidation = validation.New[AzureMonitorMetricDimension](
 	validation.ForPointer(func(a AzureMonitorMetricDimension) *string { return a.Name }).
@@ -103,3 +111,39 @@ var azureMonitorMetricDimensionValidation = validation.New[AzureMonitorMetricDim
 			validation.StringMaxLength(255),
 			validation.StringASCII()),
 )
+
+var azureMonitorCountMetricsLevelValidation = validation.New[CountMetricsSpec](
+	validation.For(validation.GetSelf[CountMetricsSpec]()).Rules(
+		validation.NewSingleRule(func(c CountMetricsSpec) error {
+			total := c.TotalMetric
+			good := c.GoodMetric
+			bad := c.BadMetric
+
+			if total == nil {
+				return nil
+			}
+			if good != nil {
+				if good.AzureMonitor.DataType != total.AzureMonitor.DataType {
+					return countMetricsPropertyEqualityError("azureMonitor.dataType", goodMetric)
+				}
+				if good.AzureMonitor.MetricNamespace != total.AzureMonitor.MetricNamespace {
+					return countMetricsPropertyEqualityError("azureMonitor.metricNamespace", goodMetric)
+				}
+				if good.AzureMonitor.ResourceID != total.AzureMonitor.ResourceID {
+					return countMetricsPropertyEqualityError("azureMonitor.resourceId", goodMetric)
+				}
+			}
+			if bad != nil {
+				if bad.AzureMonitor.DataType != total.AzureMonitor.DataType {
+					return countMetricsPropertyEqualityError("azureMonitor.dataType", badMetric)
+				}
+				if bad.AzureMonitor.MetricNamespace != total.AzureMonitor.MetricNamespace {
+					return countMetricsPropertyEqualityError("azureMonitor.metricNamespace", badMetric)
+				}
+				if bad.AzureMonitor.ResourceID != total.AzureMonitor.ResourceID {
+					return countMetricsPropertyEqualityError("azureMonitor.resourceId", badMetric)
+				}
+			}
+			return nil
+		}).WithErrorCode(validation.ErrorCodeNotEqualTo)),
+).When(whenCountMetricsIs(v1alpha.AzureMonitor))
