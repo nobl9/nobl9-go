c083be57eee2d0f0aa826bc2fac7e1bebd237f4f
daniel.zelazny <daniel.zelazny@nobl9.com>
2023-11-23 16:31:21 +0100

    PC-10133 Define examples

---
+++ manifest/v1alpha/annotation/example_test.go
@@ -0,0 +1,48 @@
+package annotation_test
+
+import (
+	"context"
+	"log"
+
+	"github.com/nobl9/nobl9-go/internal/examples"
+	"github.com/nobl9/nobl9-go/manifest"
+	"github.com/nobl9/nobl9-go/manifest/v1alpha/annotation"
+)
+
+func ExampleAnnotation() {
+	// Create the object:
+	myAnnotation := annotation.New(
+		annotation.Metadata{
+			Name:    "my-annotation",
+			Project: "my-project",
+		},
+		annotation.Spec{
+			Slo:           "existing-slo",
+			ObjectiveName: "existing-slo-objective-1",
+			Description:   "Example annotation",
+			StartTime:     "2023-05-01T17:10:05Z",
+			EndTime:       "2023-05-02T17:10:05Z",
+		},
+	)
+	// Verify the object:
+	if err := myAnnotation.Validate(); err != nil {
+		log.Fatalf("annotation validation failed, err: %v", err)
+	}
+	// Apply the object:
+	client := examples.GetOfflineEchoClient()
+	if err := client.ApplyObjects(context.Background(), []manifest.Object{myAnnotation}); err != nil {
+		log.Fatalf("failed to apply annotation, err: %v", err)
+	}
+	// Output:
+	// apiVersion: n9/v1alpha
+	// kind: Annotation
+	// metadata:
+	//   name: my-annotation
+	//   project: my-project
+	// spec:
+	//   slo: existing-slo
+	//   objectiveName: existing-slo-objective-1
+	//   description: Example annotation
+	//   startTime: 2023-05-01T17:10:05Z
+	//   endTime: 2023-05-02T17:10:05Z
+}
