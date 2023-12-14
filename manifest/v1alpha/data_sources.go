63c6b5c848c1e1cb18a990615dc680a39fb53f07
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2023-12-14 12:40:36 +0100

    bring back DurationUnitFormString

--- manifest/v1alpha/data_sources.go
+++ manifest/v1alpha/data_sources.go
@@ -266,6 +266,16 @@ func (d Duration) Duration() time.Duration {
 	return value * d.Unit.Duration()
 }
 
+func DurationUnitFromString(unit string) (DurationUnit, error) {
+	switch cases.Title(language.Und).String(unit) {
+	case string(Minute), minuteAlias:
+		return Minute, nil
+	case string(Second), secondAlias:
+		return Second, nil
+	}
+	return Second, errors.Errorf("'%s' is not a valid DurationUnit", unit)
+}
+
 func (d DurationUnit) Duration() time.Duration {
 	switch d {
 	case Millisecond:
