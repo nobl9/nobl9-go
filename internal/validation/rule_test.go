7bebabf7dc316b955f1afd0bf4faa1fe86cfcd73
Nikodem Rafalski <nikodem.rafalski@nobl9.com>
2024-03-28 14:37:46 +0100

    validation: WithMessage rule extension

--- internal/validation/rule_test.go
+++ internal/validation/rule_test.go
@@ -36,6 +36,50 @@ func TestSingleRule_WithErrorCode(t *testing.T) {
 	assert.Equal(t, "test", err.(*RuleError).Code)
 }
 
+func TestSingleRule_WithMessage(t *testing.T) {
+	for _, test := range []struct {
+		Error         string
+		Message       string
+		Details       string
+		ExpectedError string
+	}{
+		{
+			Error:         "this is error",
+			Message:       "",
+			Details:       "details",
+			ExpectedError: "this is error; details",
+		},
+		{
+			Error:         "this is error",
+			Message:       "this is message",
+			Details:       "",
+			ExpectedError: "this is message",
+		},
+		{
+			Error:         "",
+			Message:       "message",
+			Details:       "details",
+			ExpectedError: "message; details",
+		},
+	} {
+		r := NewSingleRule[int](func(v int) error {
+			if v < 0 {
+				return errors.Errorf(test.Error)
+			}
+			return nil
+		}).
+			WithErrorCode("test").
+			WithMessage(test.Message).
+			WithDetails(test.Details)
+
+		err := r.Validate(0)
+		assert.Nil(t, err)
+		err = r.Validate(-1)
+		assert.EqualError(t, err, test.ExpectedError)
+		assert.Equal(t, "test", err.(*RuleError).Code)
+	}
+}
+
 func TestSingleRule_WithDetails(t *testing.T) {
 	for _, test := range []struct {
 		Error         string
