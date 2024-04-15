905a269d6e89663d828257f2c4ddfdf5d1ef4437
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2024-04-15 17:40:30 +0200

    add another log

--- manifest/v1alpha/parser/parser.go
+++ manifest/v1alpha/parser/parser.go
@@ -119,7 +119,6 @@ func getUnmarshalFunc(data []byte, format manifest.ObjectFormat) (unmarshalFunc,
 			if UseStrictDecodingMode {
 				opts = append(opts, yaml.Strict())
 			}
-			fmt.Println("data", string(data))
 			return yaml.UnmarshalWithOptions(data, v, opts...)
 		}
 	default:
