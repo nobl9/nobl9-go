9c66f0c581210c028a85ede020a3f3def20ee89c
dawidwisn <dawid.wisniewski@nobl9.com>
2024-05-09 11:18:17 +0200

    Revert "Revert "feat: PC-12488 PC-12490 Add LogicMonitor Direct, Agent and Metric Spec (#380)" (#398)"
    This reverts commit 5d528c0bcc853bafe637f7b4ce1ef3255a953434.

--- manifest/v1alpha/agent/validation.go
+++ manifest/v1alpha/agent/validation.go
@@ -121,6 +121,9 @@ var specValidation = validation.New[Spec](
 	validation.ForPointer(func(s Spec) *HoneycombConfig { return s.Honeycomb }).
 		WithName("honeycomb").
 		Include(honeycombValidation),
+	validation.ForPointer(func(s Spec) *LogicMonitorConfig { return s.LogicMonitor }).
+		WithName("logicMonitor").
+		Include(logicMonitorValidation),
 )
 
 var (
@@ -191,6 +194,12 @@ var (
 			WithName("tenantId").
 			Required().
 			Rules(validation.StringUUID()),
+	)
+	logicMonitorValidation = validation.New[LogicMonitorConfig](
+		validation.For(func(l LogicMonitorConfig) string { return l.Account }).
+			WithName("account").
+			Required().
+			Rules(validation.StringNotEmpty()),
 	)
 	// URL only.
 	prometheusValidation    = newURLValidator(func(p PrometheusConfig) string { return p.URL })
@@ -354,6 +363,11 @@ var exactlyOneDataSourceTypeValidationRule = validation.NewSingleRule(func(spec 
 	}
 	if spec.Honeycomb != nil {
 		if err := typesMatch(v1alpha.Honeycomb); err != nil {
+			return err
+		}
+	}
+	if spec.LogicMonitor != nil {
+		if err := typesMatch(v1alpha.LogicMonitor); err != nil {
 			return err
 		}
 	}
--- manifest/v1alpha/slo/validation_test.go
+++ manifest/v1alpha/slo/validation_test.go
@@ -1671,6 +1671,13 @@ fetch consumed_api
 		Calculation: "SUM",
 		Attribute:   "http.status_code",
 	}},
+	v1alpha.LogicMonitor: {LogicMonitor: &LogicMonitorMetric{
+		QueryType:                  "device_metrics",
+		Account:                    "account",
+		DeviceDataSourceInstanceID: 1029,
+		GraphID:                    11354,
+		Line:                       "MAXRTT",
+	}},
 }
 
 func ptr[T any](v T) *T { return &v }
