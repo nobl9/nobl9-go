758ef323ae24cd89d7d0687bc66f553943ed9b73
Marcin Ławniczak <marcin@lawniczak.me>
2024-05-09 17:05:55 +0200

    Update manifest/v1alpha/slo/slo_test.go
    Co-authored-by: Mateusz Hawrus <48822818+nieomylnieja@users.noreply.github.com>

--- manifest/v1alpha/slo/slo_test.go
+++ manifest/v1alpha/slo/slo_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSLO_SpecHasCompositeObjectives(t *testing.T) {
+func TestSpec_HasCompositeObjectives(t *testing.T) {
 	t.Run("passes - composite SLO", func(t *testing.T) {
 		slo := validCompositeSLO()
 
