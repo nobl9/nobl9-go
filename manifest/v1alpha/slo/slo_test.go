1a39900c810143d39ef59f612a85f96060307f95
Marcin Ławniczak <marcin@lawniczak.me>
2024-05-10 11:04:41 +0200

    test: PC-12019 Add basic tests for `slo.SpecHasCompositeObjectives()` (#404)
    ## Motivation
    
    Method was not tested before.
    
    ## Summary
    
    New tests.
    
    ## Testing
    
    Added simple unit tests.
    
    ---------
    
    Co-authored-by: Mateusz Hawrus <48822818+nieomylnieja@users.noreply.github.com>

---
+++ manifest/v1alpha/slo/slo_test.go
@@ -0,0 +1,21 @@
+package slo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSpec_HasCompositeObjectives(t *testing.T) {
+	t.Run("passes - composite SLO", func(t *testing.T) {
+		slo := validCompositeSLO()
+
+		assert.Equal(t, true, slo.Spec.HasCompositeObjectives())
+	})
+
+	t.Run("passes - normal SLO", func(t *testing.T) {
+		slo := validSLO()
+
+		assert.Equal(t, false, slo.Spec.HasCompositeObjectives())
+	})
+}
