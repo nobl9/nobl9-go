0919302a94233a3cd0609cb232aa4210186a2faa
Jakub Pięta <jakub.pieta@nobl9.com>
2024-05-28 15:55:50 +0200

    PC-12952 overrides proposal for Budget Adjustment

--- manifest/v1alpha/budgetadjustment/example_test.go
+++ manifest/v1alpha/budgetadjustment/example_test.go
@@ -30,6 +30,21 @@ func ExampleBudgetAdjustment() {
 					},
 				},
 			},
+			Overrides: budgetadjustment.Overrides{
+				{
+					Date:    time.Date(2024, 2, 5, 5, 0, 0, 0, time.UTC),
+					Comment: "Example override with excluded event",
+					Exclude: true,
+				},
+				{
+					Date:    time.Date(2024, 2, 12, 5, 0, 0, 0, time.UTC),
+					Comment: "Example override with modified event",
+					Modify: budgetadjustment.Modify{
+						Duration:   "2h",
+						EventStart: time.Date(2024, 2, 12, 6, 0, 0, 0, time.UTC),
+					},
+				},
+			},
 		},
 	)
 	// Verify the object:
@@ -39,21 +54,30 @@ func ExampleBudgetAdjustment() {
 	// Apply the object:
 	client := examples.GetOfflineEchoClient()
 	if err := client.Objects().V1().Apply(context.Background(), []manifest.Object{budgetAdjustment}); err != nil {
-		log.Fatalf("failed to apply alert method, err: %v", err)
+		log.Fatalf("failed to apply budget adjustment, err: %v", err)
 	}
 	// Output:
 	// apiVersion: n9/v1alpha
 	// kind: BudgetAdjustment
 	// metadata:
-	//   name: budget-adjustment
-	//   displayName: My budget adjustment
+	//  name: budget-adjustment
+	//  displayName: My budget adjustment
 	// spec:
-	//   description: Example budget adjustment
-	//   firstEventStart: 2024-02-05T05:00:00Z
-	//   duration: 1h
-	//   rrule: FREQ=WEEKLY;INTERVAL=1
-	//   filters:
-	//     slos:
-	//     - name: slo-name
-	//       project: default
+	//  description: Example budget adjustment
+	//  firstEventStart: 2024-02-05T05:00:00Z
+	//  duration: 1h
+	//  rrule: FREQ=WEEKLY;INTERVAL=1
+	//  filters:
+	//    slos:
+	//	  - name: slo-name
+	//      project: default
+	//	overrides:
+	//	- date: 2024-02-05T05:00:00Z
+	//	  comment: Example override with excluded event
+	//    exclude: true
+	//	- date: 2024-02-12T05:00:00Z
+	//	  comment: Example override with modified event
+	//	  modify:
+	//	    duration: 2h
+	//	    eventStart: 2024-02-12T06:00:00Z
 }
