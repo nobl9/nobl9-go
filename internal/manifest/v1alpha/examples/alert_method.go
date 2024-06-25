8617c9947ddaa4b2c3f1c3f462427698d1282a2c
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2024-06-25 15:50:28 +0200

    add alertmethod

---
+++ internal/manifest/v1alpha/examples/alert_method.go
@@ -0,0 +1,9 @@
+package v1alphaExamples
+
+import v1alphaAlertMethod "github.com/nobl9/nobl9-go/manifest/v1alpha/alertmethod"
+
+type AlertMethodVariant struct {
+	AlertMethod v1alphaAlertMethod.AlertMethod
+}
+
+func AlertMethod() {}
