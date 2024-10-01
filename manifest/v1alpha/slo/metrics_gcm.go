cc19df1f9d5d6e8b08deb0ce8c1189548078ae31
Jakub Mikłasz <jakub.miklasz@nobl9.com>
2024-10-01 14:07:58 +0200

    add promql to gcm metric

--- manifest/v1alpha/slo/metrics_gcm.go
+++ manifest/v1alpha/slo/metrics_gcm.go
@@ -1,18 +1,74 @@
 package slo
 
-import "github.com/nobl9/govy/pkg/govy"
+import (
+	"github.com/nobl9/govy/pkg/govy"
+	"github.com/nobl9/govy/pkg/rules"
+	"github.com/pkg/errors"
+
+	"github.com/nobl9/nobl9-go/manifest/v1alpha"
+)
 
 // GCMMetric represents metric from GCM
 type GCMMetric struct {
-	Query     string `json:"query"`
-	ProjectID string `json:"projectId"`
+	Query       string `json:"query,omitempty"`
+	ProjectID   string `json:"projectId"`
+	PromqlQuery string `json:"promqlQuery,omitempty"`
+}
+
+// IsMQLConfiguration returns true if the metric is configured with MQL query.
+func (g GCMMetric) IsMQLConfiguration() bool {
+	return g.Query != ""
+}
+
+// IsPromqlConfiguration returns true if the metric is configured with promql query.
+func (g GCMMetric) IsPromqlConfiguration() bool {
+	return g.PromqlQuery != ""
 }
 
 var gcmValidation = govy.New[GCMMetric](
-	govy.For(func(e GCMMetric) string { return e.Query }).
-		WithName("query").
-		Required(),
 	govy.For(func(e GCMMetric) string { return e.ProjectID }).
 		WithName("projectId").
 		Required(),
+	govy.For(govy.GetSelf[GCMMetric]()).Rules(
+		govy.NewRule(func(c GCMMetric) error {
+			var configOptions int
+			if c.IsMQLConfiguration() {
+				configOptions++
+			}
+			if c.IsPromqlConfiguration() {
+				configOptions++
+			}
+			if configOptions != 1 {
+				return errors.New("exactly one of configuration option is required [query, promqlQuery]")
+			}
+			return nil
+		}).WithErrorCode(rules.ErrorCodeOneOf),
+	),
+)
+
+var gcmCountMetricsLevelValidation = govy.New[CountMetricsSpec](
+	govy.For(govy.GetSelf[CountMetricsSpec]()).Rules(
+		govy.NewRule(func(c CountMetricsSpec) error {
+			total := c.TotalMetric
+			good := c.GoodMetric
+
+			if total == nil {
+				return nil
+			}
+
+			if good != nil {
+				if total.GCM.IsPromqlConfiguration() && !good.GCM.IsPromqlConfiguration() {
+					return countMetricsPropertyEqualityError("query format [gcm.promqlQuery]", goodMetric)
+				}
+				if total.GCM.IsMQLConfiguration() && !good.GCM.IsMQLConfiguration() {
+					return countMetricsPropertyEqualityError("query format [gcm.query]", goodMetric)
+				}
+			}
+
+			return nil
+		}).WithErrorCode(rules.ErrorCodeNotEqualTo),
+	),
+).When(
+	whenCountMetricsIs(v1alpha.GCM),
+	govy.WhenDescription("countMetrics is GCM"),
 )
--- manifest/v1alpha/slo/metrics_gcm_test.go
+++ manifest/v1alpha/slo/metrics_gcm_test.go
@@ -10,27 +10,102 @@ import (
 )
 
 func TestGCM(t *testing.T) {
-	t.Run("passes", func(t *testing.T) {
-		slo := validRawMetricSLO(v1alpha.GCM)
-		err := validate(slo)
-		testutils.AssertNoError(t, slo, err)
+	t.Run("raw metrics", func(t *testing.T) {
+		t.Run("passes", func(t *testing.T) {
+			slo := validRawMetricSLO(v1alpha.GCM)
+			err := validate(slo)
+			testutils.AssertNoError(t, slo, err)
+		})
+		t.Run("required", func(t *testing.T) {
+			t.Run("projectID missing", func(t *testing.T) {
+				slo := validRawMetricSLO(v1alpha.GCM)
+				slo.Spec.Objectives[0].RawMetric.MetricQuery.GCM = &GCMMetric{
+					Query: "123",
+				}
+				err := validate(slo)
+				testutils.AssertContainsErrors(t, slo, err, 1,
+					testutils.ExpectedError{
+						Prop: "spec.objectives[0].rawMetric.query.gcm.projectId",
+						Code: rules.ErrorCodeRequired,
+					},
+				)
+			})
+			t.Run("query missing", func(t *testing.T) {
+				slo := validRawMetricSLO(v1alpha.GCM)
+				slo.Spec.Objectives[0].RawMetric.MetricQuery.GCM = &GCMMetric{
+					ProjectID: "123",
+				}
+				err := validate(slo)
+				testutils.AssertContainsErrors(t, slo, err, 1,
+					testutils.ExpectedError{
+						Prop: "spec.objectives[0].rawMetric.query.gcm",
+						Code: rules.ErrorCodeOneOf,
+					},
+				)
+			})
+		})
+		t.Run("both mql and promql defined", func(t *testing.T) {
+			slo := validRawMetricSLO(v1alpha.GCM)
+			slo.Spec.Objectives[0].RawMetric.MetricQuery.GCM = &GCMMetric{
+				ProjectID:   "123",
+				Query:       "123",
+				PromqlQuery: "123",
+			}
+			err := validate(slo)
+			testutils.AssertContainsErrors(t, slo, err, 1,
+				testutils.ExpectedError{
+					Prop: "spec.objectives[0].rawMetric.query.gcm",
+					Code: rules.ErrorCodeOneOf,
+				},
+			)
+		})
 	})
-	t.Run("required", func(t *testing.T) {
-		slo := validRawMetricSLO(v1alpha.GCM)
-		slo.Spec.Objectives[0].RawMetric.MetricQuery.GCM = &GCMMetric{
-			Query:     "",
-			ProjectID: "",
-		}
-		err := validate(slo)
-		testutils.AssertContainsErrors(t, slo, err, 2,
-			testutils.ExpectedError{
-				Prop: "spec.objectives[0].rawMetric.query.gcm.query",
-				Code: rules.ErrorCodeRequired,
-			},
-			testutils.ExpectedError{
-				Prop: "spec.objectives[0].rawMetric.query.gcm.projectId",
-				Code: rules.ErrorCodeRequired,
-			},
-		)
+	t.Run("count metrics", func(t *testing.T) {
+		t.Run("passes mql", func(t *testing.T) {
+			slo := validCountMetricSLO(v1alpha.GCM)
+			err := validate(slo)
+			testutils.AssertNoError(t, slo, err)
+		})
+		t.Run("passes promql", func(t *testing.T) {
+			slo := validCountMetricSLO(v1alpha.GCM)
+			slo.Spec.Objectives[0].CountMetrics.GoodMetric.GCM = &GCMMetric{
+				ProjectID:   "123",
+				PromqlQuery: "123",
+			}
+			slo.Spec.Objectives[0].CountMetrics.TotalMetric.GCM = &GCMMetric{
+				ProjectID:   "123",
+				PromqlQuery: "123",
+			}
+			err := validate(slo)
+			testutils.AssertNoError(t, slo, err)
+		})
+		t.Run("good is mql, total is promql", func(t *testing.T) {
+			slo := validCountMetricSLO(v1alpha.GCM)
+			slo.Spec.Objectives[0].CountMetrics.TotalMetric.GCM = &GCMMetric{
+				ProjectID:   "123",
+				PromqlQuery: "123",
+			}
+			err := validate(slo)
+			testutils.AssertContainsErrors(t, slo, err, 1,
+				testutils.ExpectedError{
+					Prop: "spec.objectives[0].countMetrics",
+					Code: rules.ErrorCodeNotEqualTo,
+				},
+			)
+		})
+		t.Run("good is promql, total is mql", func(t *testing.T) {
+			slo := validCountMetricSLO(v1alpha.GCM)
+			slo.Spec.Objectives[0].CountMetrics.GoodMetric.GCM = &GCMMetric{
+				ProjectID:   "123",
+				PromqlQuery: "123",
+			}
+			err := validate(slo)
+			testutils.AssertContainsErrors(t, slo, err, 1,
+				testutils.ExpectedError{
+					Prop: "spec.objectives[0].countMetrics",
+					Code: rules.ErrorCodeNotEqualTo,
+				},
+			)
+		})
 	})
 }
