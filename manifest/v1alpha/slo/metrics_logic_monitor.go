6db3273447a3be932b255d8c7990e2a18ec3db4a
dawidwisn <dawid.wisniewski@nobl9.com>
2024-11-20 09:14:25 +0100

    PC-14860 Support Website metrics in LogicMonitor

--- manifest/v1alpha/slo/metrics_logic_monitor.go
+++ manifest/v1alpha/slo/metrics_logic_monitor.go
@@ -3,31 +3,90 @@ package slo
 import (
 	"github.com/nobl9/govy/pkg/govy"
 	"github.com/nobl9/govy/pkg/rules"
+	"github.com/pkg/errors"
+)
+
+const (
+	LMQueryTypeDeviceMetrics  = "device_metrics"
+	LMQueryTypeWebsiteMetrics = "website_metrics"
 )
 
 // LogicMonitorMetric represents metric from LogicMonitor
 type LogicMonitorMetric struct {
-	QueryType                  string `json:"queryType"`
-	DeviceDataSourceInstanceID int    `json:"deviceDataSourceInstanceId"`
-	GraphID                    int    `json:"graphId"`
-	Line                       string `json:"line"`
+	QueryType string `json:"queryType"`
+	Line      string `json:"line"`
+	// QueryType = device_metrics
+	DeviceDataSourceInstanceID int `json:"deviceDataSourceInstanceId"`
+	GraphID                    int `json:"graphId"`
+	// QueryType = website_metrics
+	WebsiteID    string `json:"websiteId"`
+	CheckpointID string `json:"checkpointId"`
+	GraphName    string `json:"graphName"`
+}
+
+func (e LogicMonitorMetric) IsDeviceMetric() bool {
+	return e.QueryType == LMQueryTypeDeviceMetrics
+}
+
+func (e LogicMonitorMetric) IsWebsiteMetric() bool {
+	return e.QueryType == LMQueryTypeWebsiteMetrics
 }
 
 var logicMonitorValidation = govy.New[LogicMonitorMetric](
 	govy.For(func(e LogicMonitorMetric) string { return e.QueryType }).
 		WithName("queryType").
 		Required().
-		Rules(rules.StringContains("device_metrics")),
+		Rules(rules.OneOf(LMQueryTypeDeviceMetrics, LMQueryTypeWebsiteMetrics)),
 	govy.For(func(e LogicMonitorMetric) int { return e.DeviceDataSourceInstanceID }).
 		WithName("deviceDataSourceInstanceId").
-		Required().
+		When(
+			func(e LogicMonitorMetric) bool { return e.IsDeviceMetric() },
+		).
 		Rules(rules.GTE(0)),
 	govy.For(func(e LogicMonitorMetric) int { return e.GraphID }).
 		WithName("graphId").
-		Required().
+		When(
+			func(e LogicMonitorMetric) bool { return e.IsDeviceMetric() },
+		).
 		Rules(rules.GTE(0)),
+	govy.For(func(e LogicMonitorMetric) string { return e.WebsiteID }).
+		WithName("websiteId").
+		When(
+			func(e LogicMonitorMetric) bool { return e.IsWebsiteMetric() },
+		).
+		Rules(rules.StringNotEmpty()),
+	govy.For(func(e LogicMonitorMetric) string { return e.CheckpointID }).
+		WithName("checkpointId").
+		When(
+			func(e LogicMonitorMetric) bool { return e.IsWebsiteMetric() },
+		).
+		Rules(rules.StringNotEmpty()),
+	govy.For(func(e LogicMonitorMetric) string { return e.GraphName }).
+		WithName("graphName").
+		When(
+			func(e LogicMonitorMetric) bool { return e.IsWebsiteMetric() },
+		).
+		Rules(rules.StringNotEmpty()),
 	govy.For(func(e LogicMonitorMetric) string { return e.Line }).
 		WithName("line").
-		Required().
+		When(
+			func(e LogicMonitorMetric) bool { return e.IsDeviceMetric() || e.IsWebsiteMetric() },
+		).
 		Rules(rules.StringNotEmpty()),
+	govy.For(govy.GetSelf[LogicMonitorMetric]()).
+		When(func(c LogicMonitorMetric) bool { return c.IsWebsiteMetric() }).
+		Rules(govy.NewRule(func(e LogicMonitorMetric) error {
+			if e.DeviceDataSourceInstanceID != 0 || e.GraphID != 0 {
+				return errors.New("deviceDataSourceInstanceId and graphId must be empty for website_metrics")
+			}
+			return nil
+		})),
+	govy.For(govy.GetSelf[LogicMonitorMetric]()).
+		When(func(c LogicMonitorMetric) bool { return c.IsDeviceMetric() }).
+		Rules(govy.NewRule(func(e LogicMonitorMetric) error {
+			if len(e.GraphName) > 0 || len(e.CheckpointID) > 0 || len(e.WebsiteID) > 0 {
+				return errors.New("graphName, checkpointId and websiteId must be empty for device_metrics")
+			}
+			return nil
+		})),
 )
--- manifest/v1alpha/slo/metrics_logic_monitor_test.go
+++ manifest/v1alpha/slo/metrics_logic_monitor_test.go
@@ -19,21 +19,9 @@ func TestLogicMonitor(t *testing.T) {
 		slo := validRawMetricSLO(v1alpha.LogicMonitor)
 		slo.Spec.Objectives[0].RawMetric.MetricQuery.LogicMonitor = &LogicMonitorMetric{}
 		err := validate(slo)
-		testutils.AssertContainsErrors(t, slo, err, 4,
+		testutils.AssertContainsErrors(t, slo, err, 2,
 			testutils.ExpectedError{
 				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.queryType",
-				Code: rules.ErrorCodeRequired,
-			},
-			testutils.ExpectedError{
-				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.deviceDataSourceInstanceId",
-				Code: rules.ErrorCodeRequired,
-			},
-			testutils.ExpectedError{
-				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.graphId",
-				Code: rules.ErrorCodeRequired,
-			},
-			testutils.ExpectedError{
-				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.line",
 				Code: rules.ErrorCodeRequired,
 			},
 		)
@@ -41,16 +29,44 @@ func TestLogicMonitor(t *testing.T) {
 	t.Run("invalid fields", func(t *testing.T) {
 		slo := validRawMetricSLO(v1alpha.LogicMonitor)
 		slo.Spec.Objectives[0].RawMetric.MetricQuery.LogicMonitor = &LogicMonitorMetric{
-			QueryType:                  "wrong-type",
-			DeviceDataSourceInstanceID: -1,
-			GraphID:                    -1,
-			Line:                       "",
+			QueryType: "wrong-type",
+			Line:      "",
 		}
 		err := validate(slo)
-		testutils.AssertContainsErrors(t, slo, err, 4,
+		testutils.AssertContainsErrors(t, slo, err, 1,
 			testutils.ExpectedError{
-				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.queryType",
-				Code: rules.ErrorCodeStringContains,
+				Prop:    "spec.objectives[0].rawMetric.query.logicMonitor.queryType",
+				Code:    rules.ErrorCodeOneOf,
+				Message: "must be one of [device_metrics, website_metrics]",
+			},
+		)
+	})
+	t.Run("line required for device_metrics", func(t *testing.T) {
+		slo := validRawMetricSLO(v1alpha.LogicMonitor)
+		slo.Spec.Objectives[0].RawMetric.MetricQuery.LogicMonitor = &LogicMonitorMetric{
+			QueryType: LMQueryTypeDeviceMetrics,
+		}
+		err := validate(slo)
+		testutils.AssertContainsErrors(t, slo, err, 1,
+
+			testutils.ExpectedError{
+				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.line",
+				Code: rules.ErrorCodeStringNotEmpty,
+			},
+		)
+	})
+	t.Run("invalid parameters for device_metrics", func(t *testing.T) {
+		slo := validRawMetricSLO(v1alpha.LogicMonitor)
+		slo.Spec.Objectives[0].RawMetric.MetricQuery.LogicMonitor = &LogicMonitorMetric{
+			QueryType:                  LMQueryTypeDeviceMetrics,
+			DeviceDataSourceInstanceID: -1,
+			GraphID:                    -1,
+		}
+		err := validate(slo)
+		testutils.AssertContainsErrors(t, slo, err, 3,
+			testutils.ExpectedError{
+				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.line",
+				Code: rules.ErrorCodeStringNotEmpty,
 			},
 			testutils.ExpectedError{
 				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.deviceDataSourceInstanceId",
@@ -60,9 +76,68 @@ func TestLogicMonitor(t *testing.T) {
 				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.graphId",
 				Code: rules.ErrorCodeGreaterThanOrEqualTo,
 			},
+		)
+	})
+	t.Run("required parameters for website_metrics", func(t *testing.T) {
+		slo := validRawMetricSLO(v1alpha.LogicMonitor)
+		slo.Spec.Objectives[0].RawMetric.MetricQuery.LogicMonitor = &LogicMonitorMetric{
+			QueryType: LMQueryTypeWebsiteMetrics,
+		}
+		err := validate(slo)
+		testutils.AssertContainsErrors(t, slo, err, 4,
 			testutils.ExpectedError{
 				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.line",
-				Code: rules.ErrorCodeRequired,
+				Code: rules.ErrorCodeStringNotEmpty,
+			},
+			testutils.ExpectedError{
+				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.websiteId",
+				Code: rules.ErrorCodeStringNotEmpty,
+			},
+			testutils.ExpectedError{
+				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.checkpointId",
+				Code: rules.ErrorCodeStringNotEmpty,
+			},
+			testutils.ExpectedError{
+				Prop: "spec.objectives[0].rawMetric.query.logicMonitor.graphName",
+				Code: rules.ErrorCodeStringNotEmpty,
+			},
+		)
+	})
+	t.Run("invalid DeviceDataSourceInstanceID and GraphID for website_metrics", func(t *testing.T) {
+		slo := validRawMetricSLO(v1alpha.LogicMonitor)
+		slo.Spec.Objectives[0].RawMetric.MetricQuery.LogicMonitor = &LogicMonitorMetric{
+			QueryType:                  LMQueryTypeWebsiteMetrics,
+			WebsiteID:                  "1",
+			CheckpointID:               "1",
+			GraphName:                  "MaxPoints",
+			Line:                       "MAX",
+			DeviceDataSourceInstanceID: 1,
+			GraphID:                    1,
+		}
+		err := validate(slo)
+		testutils.AssertContainsErrors(t, slo, err, 1,
+			testutils.ExpectedError{
+				Prop:    "spec.objectives[0].rawMetric.query.logicMonitor",
+				Message: "deviceDataSourceInstanceId and graphId must be empty for website_metrics",
+			},
+		)
+	})
+	t.Run("invalid WebsiteID/CheckpointID/GraphName for device_metrics", func(t *testing.T) {
+		slo := validRawMetricSLO(v1alpha.LogicMonitor)
+		slo.Spec.Objectives[0].RawMetric.MetricQuery.LogicMonitor = &LogicMonitorMetric{
+			QueryType:                  LMQueryTypeDeviceMetrics,
+			WebsiteID:                  "1",
+			CheckpointID:               "1",
+			GraphName:                  "MaxPoints",
+			Line:                       "MAX",
+			DeviceDataSourceInstanceID: 1,
+			GraphID:                    1,
+		}
+		err := validate(slo)
+		testutils.AssertContainsErrors(t, slo, err, 1,
+			testutils.ExpectedError{
+				Prop:    "spec.objectives[0].rawMetric.query.logicMonitor",
+				Message: "graphName, checkpointId and websiteId must be empty for device_metrics",
 			},
 		)
 	})
