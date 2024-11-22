81ad8e2ea53cb3f21407a94afa408ad3d548fb82
Adam Szymanski <adam.szymanski@nobl9.com>
2024-11-22 16:57:46 +0100

    Remove Status from AlertMethod

--- manifest/v1alpha/alertmethod/alert_method.go
+++ manifest/v1alpha/alertmethod/alert_method.go
@@ -26,7 +26,6 @@ type AlertMethod struct {
 	Kind       manifest.Kind    `json:"kind"`
 	Metadata   Metadata         `json:"metadata"`
 	Spec       Spec             `json:"spec"`
-	Status     *Status          `json:"status,omitempty"`
 
 	Organization   string `json:"organization,omitempty"`
 	ManifestSource string `json:"manifestSrc,omitempty"`
@@ -37,12 +36,6 @@ type Metadata struct {
 	Name        string `json:"name"`
 	DisplayName string `json:"displayName,omitempty"`
 	Project     string `json:"project,omitempty"`
-}
-
-// Status represents content of Status optional for AlertMethod Object
-type Status struct {
-	LastTestDate       string `json:"lastTestDate,omitempty"`
-	NextTestPossibleAt string `json:"nextTestPossibleAt,omitempty"`
 }
 
 // Spec holds detailed information specific to AlertMethod.
