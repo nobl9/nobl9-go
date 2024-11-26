c4b1c21b2c37a17cdca92ec5bad79cd5b2b43a01
lukasz-dobek <lukasz.dobek@nobl9.com>
2024-11-26 11:45:29 +0100

    Add test

--- manifest/v1alpha/slo/metrics_test.go
+++ manifest/v1alpha/slo/metrics_test.go
@@ -21,3 +21,41 @@ func TestQuery(t *testing.T) {
 		assert.NotEmpty(t, spec)
 	}
 }
+
+func TestFormatRawJSONMetricQueryToString(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			"empty string",
+			``,
+			``,
+		},
+		{
+			"empty string",
+			`invalidjson:"`,
+			``,
+		},
+		{
+			"cloudwatch standard",
+			`{"stat": "Average", "region": "eu-central-1", "namespace": "asd", "dimensions": [{"name": "asd", "value": "zcx"}], "metricName": "ads"}`,
+			"Dimensions: \n 1:\n  Name: asd\n  Value: zcx\nMetricname: ads\nNamespace: asd\nRegion: eu-central-1\nStat: Average\n",
+		},
+		{
+			"cloudwatch json",
+			`{"json": "[\n    {\n        \"Id\": \"e1\",\n        \"Expression\": \"m1 / m2\",\n        \"Period\": 60\n    },\n    {\n        \"Id\": \"m1\",\n        \"MetricStat\": {\n            \"Metric\": {\n                \"Namespace\": \"AWS/ApplicationELB\",\n                \"MetricName\": \"HTTPCode_Target_2XX_Count\",\n                \"Dimensions\": [\n                    {\n                        \"Name\": \"LoadBalancer\",\n                        \"Value\": \"app/main-default-appingress-350b/904311bedb964754\"\n                    }\n                ]\n            },\n            \"Period\": 60,\n            \"Stat\": \"SampleCount\"\n        },\n        \"ReturnData\": false\n    },\n    {\n        \"Id\": \"m2\",\n        \"MetricStat\": {\n            \"Metric\": {\n                \"Namespace\": \"AWS/ApplicationELB\",\n                \"MetricName\": \"RequestCount\",\n                \"Dimensions\": [\n                    {\n                        \"Name\": \"LoadBalancer\",\n                        \"Value\": \"app/main-default-appingress-350b/904311bedb964754\"\n                    }\n                ]\n            },\n            \"Period\": 60,\n            \"Stat\": \"SampleCount\"\n        },\n        \"ReturnData\": false\n    }\n]", "region": "eu-central-1"}`,
+			"Json: [\n    {\n        \"Id\": \"e1\",\n        \"Expression\": \"m1 / m2\",\n        \"Period\": 60\n    },\n    {\n        \"Id\": \"m1\",\n        \"MetricStat\": {\n            \"Metric\": {\n                \"Namespace\": \"AWS/ApplicationELB\",\n                \"MetricName\": \"HTTPCode_Target_2XX_Count\",\n                \"Dimensions\": [\n                    {\n                        \"Name\": \"LoadBalancer\",\n                        \"Value\": \"app/main-default-appingress-350b/904311bedb964754\"\n                    }\n                ]\n            },\n            \"Period\": 60,\n            \"Stat\": \"SampleCount\"\n        },\n        \"ReturnData\": false\n    },\n    {\n        \"Id\": \"m2\",\n        \"MetricStat\": {\n            \"Metric\": {\n                \"Namespace\": \"AWS/ApplicationELB\",\n                \"MetricName\": \"RequestCount\",\n                \"Dimensions\": [\n                    {\n                        \"Name\": \"LoadBalancer\",\n                        \"Value\": \"app/main-default-appingress-350b/904311bedb964754\"\n                    }\n                ]\n            },\n            \"Period\": 60,\n            \"Stat\": \"SampleCount\"\n        },\n        \"ReturnData\": false\n    }\n]\nRegion: eu-central-1\n",
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			got := formatRawJSONMetricQueryToString([]byte(tc.input))
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
