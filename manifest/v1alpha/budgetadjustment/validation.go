e02dc419104a3f39ecc9283b762229062237b493
Jakub Cegiełka <kuba.ceg@gmail.com>
2025-01-23 13:33:54 +0100

    feat: PC-14241 budget adjustment spec duration should not have precision set to 1 m (#632)
    ## Motivation
    
    Due to customer question we have to set duration precision to one second
    with minimum one minute limit.
    
    ---------
    
    Co-authored-by: kubaceg <jakub.cegielka@nobl9.com>

--- manifest/v1alpha/budgetadjustment/validation.go
+++ manifest/v1alpha/budgetadjustment/validation.go
@@ -46,7 +46,7 @@ var specValidation = govy.New[Spec](
 	govy.Transform(func(s Spec) string { return s.Duration }, time.ParseDuration).
 		WithName("duration").
 		Required().
-		Rules(rules.DurationPrecision(time.Minute)),
+		Rules(durationPrecision),
 	govy.Transform(func(s Spec) string { return s.Rrule }, rrule.StrToRRule).
 		WithName("rrule").
 		Rules(atLeastHourlyFreq),
@@ -103,6 +103,17 @@ var atLeastHourlyFreq = govy.NewRule(func(rule *rrule.RRule) error {
 var secondTimePrecision = govy.NewRule(func(t time.Time) error {
 	if t.Nanosecond() != 0 {
 		return errors.New("time must be defined with 1s precision")
+	}
+
+	return nil
+})
+
+var durationPrecision = govy.NewRule(func(t time.Duration) error {
+	if t.Minutes() < 1 {
+		return errors.New("duration must be at least 1 minute")
+	}
+	if t.Nanoseconds()%int64(time.Second) != 0 {
+		return errors.New("duration must be defined with 1s precision")
 	}
 
 	return nil
