948c3918e1c92017443d6ee830bd348cbf93213e
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2025-02-05 10:11:55 +0100

    improve doc

--- internal/stringutils/stringutils.go
+++ internal/stringutils/stringutils.go
@@ -2,5 +2,7 @@
 
 package stringutils
 
-// RemoveCR by default does nothing, it only modifies the string if built for Windows.
+// RemoveCR by default does nothing.
+// It only modifies the string if the binary is built for Windows.
+// See stringutils_windows.go for more details.
 func RemoveCR(s string) string { return s }
