d24fa2846f159b9ac709b159c9500fae4b6164ce
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2025-05-28 13:09:06 +0200

    adjust e2e tests

--- tests/users_test.go
+++ tests/users_test.go
@@ -14,13 +14,13 @@ func Test_Users_V2_GetUser(t *testing.T) {
 	t.Parallel()
 	ctx := context.Background()
 
-	userEmail, err := client.GetUser(ctx)
+	userID, err := client.GetUserID(ctx)
 	require.NoError(t, err)
 
-	user, err := client.Users().V2().GetUser(ctx, userEmail)
+	user, err := client.Users().V2().GetUser(ctx, userID)
 	require.NoError(t, err)
-	assert.NotEmpty(t, user.UserID)
+	assert.NotEmpty(t, user.Email)
 	assert.NotEmpty(t, user.FirstName)
 	assert.NotEmpty(t, user.LastName)
-	assert.Equal(t, userEmail, user.Email)
+	assert.Equal(t, userID, user.UserID)
 }
