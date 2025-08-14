63dfcb830c7ec898af25e4e6d9853339f01c8c5d
Piotr Kazulak <piotr.kazulak@nobl9.com>
2025-08-14 16:52:52 +0200

    Add submit SLO review request payload

---
+++ sdk/endpoints/objects/v1/slos/review/review.go
@@ -0,0 +1,30 @@
+package review
+
+import (
+	"github.com/nobl9/govy/pkg/govy"
+	"github.com/nobl9/govy/pkg/rules"
+)
+
+const (
+	ReviewStatusPending = "pending"
+	ReviewStatusReviewd = "reviewed"
+	ReviewStatusSkipped = "skipped"
+)
+
+type SubmitReviewPayload struct {
+	Status string `json:"status"`
+	Note   string `json:"note,omitempty"`
+}
+
+func (p SubmitReviewPayload) Validate() error {
+	return validator.Validate(p)
+}
+
+var validator = govy.New(
+	govy.For(func(r SubmitReviewPayload) string { return r.Status }).
+		Required().
+		Rules(rules.OneOf(ReviewStatusSkipped, ReviewStatusPending, ReviewStatusReviewd)),
+	govy.For(func(r SubmitReviewPayload) string { return r.Note }).
+		OmitEmpty().
+		Rules(rules.StringMaxLength(500)),
+)
