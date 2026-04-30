b12f5a6ac8641f35428d15bfa7962f4113b8751f
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2026-04-30 14:03:38 +0200

    fix: preserve SDK headers for Prometheus requests

--- sdk/prometheus.go
+++ sdk/prometheus.go
@@ -1,10 +1,14 @@
 package sdk
 
 import (
+	"bytes"
 	"context"
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
 	"sync"
 
-	promapi "github.com/prometheus/client_golang/api"
 	promv1 "github.com/prometheus/client_golang/api/prometheus/v1"
 
 	prometheusEndpoints "github.com/nobl9/nobl9-go/sdk/endpoints/prometheus"
@@ -16,17 +20,14 @@ func (c *Client) Prometheus() prometheusEndpoints.Versions {
 	return prometheusEndpoints.NewVersions(c.newPrometheusAPI)
 }
 
-func (c *Client) newPrometheusAPI(ctx context.Context) (prometheusEndpointsV1.API, error) {
+func (c *Client) newPrometheusAPI(context.Context) (prometheusEndpointsV1.API, error) {
 	c.prometheusAPI.mu.Lock()
 	defer c.prometheusAPI.mu.Unlock()
 
 	if c.prometheusAPI.api != nil {
 		return c.prometheusAPI.api, nil
 	}
-	api, err := c.createPrometheusAPI(ctx)
-	if err != nil {
-		return nil, err
-	}
+	api := promv1.NewAPI(prometheusClient{client: c})
 	c.prometheusAPI.api = api
 	return api, nil
 }
@@ -36,17 +37,60 @@ type prometheusAPIStore struct {
 	api prometheusEndpointsV1.API
 }
 
-func (c *Client) createPrometheusAPI(ctx context.Context) (prometheusEndpointsV1.API, error) {
-	apiURL, err := c.getAPIURL(ctx)
-	if err != nil {
-		return nil, err
+type prometheusClient struct {
+	client *Client
+}
+
+func (c prometheusClient) URL(ep string, args map[string]string) *url.URL {
+	p := path.Join("prometheus", "v1", ep)
+	for arg, val := range args {
+		p = strings.ReplaceAll(p, ":"+arg, val)
 	}
-	client, err := promapi.NewClient(promapi.Config{
-		Address: apiURL.JoinPath("prometheus", "v1").String(),
-		Client:  c.HTTP,
-	})
-	if err != nil {
-		return nil, err
+	return &url.URL{Path: p}
+}
+
+func (c prometheusClient) Do(ctx context.Context, req *http.Request) (*http.Response, []byte, error) {
+	requestCtx := ctx
+	if requestCtx == nil {
+		requestCtx = req.Context()
 	}
-	return promv1.NewAPI(client), nil
+	if requestCtx == nil {
+		requestCtx = context.Background()
+	}
+	sdkReq, err := c.client.CreateRequest(
+		requestCtx,
+		req.Method,
+		req.URL.Path,
+		req.Header.Clone(),
+		req.URL.Query(),
+		req.Body,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+	sdkReq.GetBody = req.GetBody
+	sdkReq.ContentLength = req.ContentLength
+	resp, err := c.client.HTTP.Do(sdkReq)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var body []byte
+	done := make(chan error, 1)
+	go func() {
+		var buf bytes.Buffer
+		_, readErr := buf.ReadFrom(resp.Body)
+		body = buf.Bytes()
+		done <- readErr
+	}()
+
+	select {
+	case <-requestCtx.Done():
+		_ = resp.Body.Close()
+		<-done
+		return resp, nil, requestCtx.Err()
+	case err := <-done:
+		_ = resp.Body.Close()
+		return resp, body, err
+	}
 }
