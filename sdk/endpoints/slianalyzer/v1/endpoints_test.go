0612773d63eaafb181f2765a7b049a39106594ab
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2026-09-21 14:30:09 +0200

    test: fix SLI Analyzer import alias spelling

--- sdk/endpoints/slianalyzer/v1/endpoints_test.go
+++ sdk/endpoints/slianalyzer/v1/endpoints_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/nobl9/nobl9-go/manifest"
 	v1alphaSLO "github.com/nobl9/nobl9-go/manifest/v1alpha/slo"
 	"github.com/nobl9/nobl9-go/sdk"
-	slianalyzerv1 "github.com/nobl9/nobl9-go/sdk/endpoints/slianalyzer/v1"
+	slianalyzerV1 "github.com/nobl9/nobl9-go/sdk/endpoints/slianalyzer/v1"
 )
 
 func TestSLIAnalyzer_CanceledRequests(t *testing.T) {
@@ -24,16 +24,16 @@ func TestSLIAnalyzer_CanceledRequests(t *testing.T) {
 		{
 			name: "create with malformed count metrics",
 			request: func(ctx context.Context, client *sdk.Client) error {
-				_, err := client.SLIAnalyzer().V1().CreateAnalysis(ctx, slianalyzerv1.CreateAnalysisRequest{
-					Metadata: slianalyzerv1.AnalysisMetadata{DisplayName: "Analysis", Project: "test"},
-					MetricSpec: slianalyzerv1.AnalysisMetricSpec{
+				_, err := client.SLIAnalyzer().V1().CreateAnalysis(ctx, slianalyzerV1.CreateAnalysisRequest{
+					Metadata: slianalyzerV1.AnalysisMetadata{DisplayName: "Analysis", Project: "test"},
+					MetricSpec: slianalyzerV1.AnalysisMetricSpec{
 						Kind: manifest.KindAgent, MetricSource: "prometheus",
 						CountMetrics: &v1alphaSLO.CountMetricsSpec{
 							GoodMetric:  &v1alphaSLO.MetricSpec{BigQuery: &v1alphaSLO.BigQueryMetric{}},
 							TotalMetric: &v1alphaSLO.MetricSpec{},
 						},
 					},
-					Period: slianalyzerv1.AnalysisPeriod{
+					Period: slianalyzerV1.AnalysisPeriod{
 						StartTime: "2026-09-01 00:00:00", EndTime: "2026-09-01 00:05:00", TimeZone: "UTC",
 					},
 				})
@@ -43,14 +43,14 @@ func TestSLIAnalyzer_CanceledRequests(t *testing.T) {
 		{
 			name: "update with missing fields",
 			request: func(ctx context.Context, client *sdk.Client) error {
-				return client.SLIAnalyzer().V1().UpdateAnalysis(ctx, "analysis", slianalyzerv1.UpdateAnalysisRequest{})
+				return client.SLIAnalyzer().V1().UpdateAnalysis(ctx, "analysis", slianalyzerV1.UpdateAnalysisRequest{})
 			},
 		},
 		{
 			name: "calculation with invalid target",
 			request: func(ctx context.Context, client *sdk.Client) error {
 				return client.SLIAnalyzer().V1().CreateCalculation(ctx, "test", "analysis",
-					slianalyzerv1.CreateCalculationRequest{BudgetTarget: 1, BudgetingMethod: "Occurrences", Operator: "lte"},
+					slianalyzerV1.CreateCalculationRequest{BudgetTarget: 1, BudgetingMethod: "Occurrences", Operator: "lte"},
 				)
 			},
 		},
